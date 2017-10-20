62255413899da1fde10dc90fc8f74896e80e1dbb
Cosmo Myzrail Gorynych <admin@nersta.ru>
2017-10-21 00:25:35 +1200

    🔨 (ct.random) better random - ct.random.range now returns float values - style fixes

--- app/ct.libs/random.js
+++ app/ct.libs/random.js
@@ -1,33 +1,32 @@
-/***************************************
-
-          [ random cotomod ]
-
-   [ (c) Cosmo Myzrail Gorynych 2013 ]
-
-***************************************/
+/*
+   [ random cotomod ]
+   [ (c) Cosmo Myzrail Gorynych 2013, 2017 ]
+*/
 
 ct.random = function (x) {
     return Math.random()*x;
 };
 ct.u.ext(ct.random,{
-    'dice': function () {
+    dice() {
         return arguments[Math.floor(Math.random() * arguments.length)];
     },
-    'range': function (x1,x2) {
-        return x1+Math.floor(Math.random() * (x2-x1));
+    range(x1,x2) {
+        return x1 + Math.random() * (x2-x1);
     },
-    'deg': function () {
+    deg() {
         return Math.random()*360;
     },
-    'coord': function () {
+    coord() {
         return [Math.floor(Math.random()*ct.width),Math.floor(Math.random()*ct.height)];
     },
-    'chance': function (x,y) {
-        if (y) return (Math.random()*y < x);
-        else return (Math.random()*100 < x);
+    chance(x,y) {
+        if (y) {
+            return (Math.random()*y < x);
+        }
+        return (Math.random()*100 < x);
     },
-    'from': function (arr) {
-        return arr[~~(Math.random()*arr.length)];
+    from(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
     }
 });
 
