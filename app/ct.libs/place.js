cea4f7ec3564d13ab07367e06385fbf08682cc6a
Cosmo Myzrail Gorynych <admin@nersta.ru>
2018-03-23 20:46:29 +1200

    ♻️🐛⚡️ Heavily refactoring ct.place

--- app/ct.libs/place.js
+++ app/ct.libs/place.js
@@ -1,3 +1,6 @@
+/* global ct */
+/* eslint prefer-destructuring: 0 */
+
 /****************************************
          [ place cotomod ]
 [ (c) Cosmo Myzrail Gorynych 2013, 2015 ]
@@ -27,7 +30,7 @@ ct.place = {
             return (
                 ((x3-x1) * (y2-y1) - (y3-y1) * (x2-x1)) * ((x4-x1) * (y2-y1) - (y4-y1) * (x2-x1)) <= 0)
             &&
-                (((x1-x3) * (y4-y3) - (y1-y3) * (x4-x3)) * ((x2-x3) * (y4-y3) - (y2-y3) * (x4-x3) )<= 0);
+                (((x1-x3) * (y4-y3) - (y1-y3) * (x4-x3)) * ((x2-x3) * (y4-y3) - (y2-y3) * (x4-x3)) <= 0);
         },
         'circle.circle'(x1, y1, r1, x2, y2, r2) {
             // detect collision by comparison of distance between centers and sum of circle's radius
@@ -71,162 +74,165 @@ ct.place = {
             );
         },
     },
-    occupied(me, x, y, type) {
-        // ct.place.occupied(<me: Copy, x: Number, y: Number>[, type: String])
+    collide(c1, c2) {
+        // ct.place.collide(<c1: Copy, c2: Copy>)
+        // Test collision between two copies
+        if (!c1.shape && !c2.shape) {
+            return false;
+        }
+        var cType = c1.shape.type + '.' + c2.shape.type;
+        if (cType === 'rect.rect') {
+            if (ct.place.check['rect.rect'](c1.x - c1.shape.left, c1.y - c1.shape.top,c1.x + c1.shape.right,c1.y + c1.shape.bottom, c2.x - c2.shape.left, c2.y - c2.shape.top, c2.x + c2.shape.right, c2.y + c2.shape.bottom)) {
+                return true;
+            }
+        } else if (cType === 'circle.circle') {
+            if (ct.place.check['circle.circle'](c1.x,c1.y,c1.shape.r,c2.x,c2.y,c2.shape.r)) {
+                return true;
+            }
+        } else if (cType === 'circle.rect') {
+            if (ct.place.check['circle.rect'](c1.x,c1.y,c1.shape.r,c2.x - c2.shape.left, c2.y - c2.shape.top,c2.x + c2.shape.right, c2.y + c2.shape.bottom)) {
+                return true;
+            }
+        } else if (cType === 'rect.circle') {
+            if (ct.place.check['circle.rect'](c2.x ,c2.y ,c2.shape.r,c1.x - c1.shape.left, c1.y - c1.shape.top,c1.x + c1.shape.right,c1.y + c1.shape.bottom)) {
+                return true;
+            }
+        } else if (cType === 'circle.point') {
+            if (ct.place.check['circle.point'](c1.x,c1.y,c1.shape.r, c2.x, c2.y)) {
+                return true;
+            }
+        } else if (cType === 'point.circle') {
+            if (ct.place.check['circle.point'](c2.x, c2.y, c2.shape.r,c1.x,c1.y)) {
+                return true;
+            }
+        } else if (cType === 'point.point') {
+            return c1.y === c2.y && c1.x === c2.x;
+        } else if (cType === 'point.rect') {
+            if (ct.place.check['rect.point'](c2.x - c2.shape.left, c2.y - c2.shape.top, c2.x + c2.shape.right, c2.y + c2.shape.bottom,c1.x,c1.y)) {
+                return true;
+            }
+        } else if (cType === 'rect.point') {
+            if (ct.place.check['rect.point'](c1.x - c1.shape.left, c1.y - c1.shape.top,c1.x + c1.shape.right,c1.y + c1.shape.bottom, c2.x, c2.y)) {
+                return true;
+            }
+        }
+        return false;
+    },
+    occupied(me, x, y, ctype) {
+        // ct.place.occupied(<me: Copy, x: Number, y: Number>[, ctype: String])
         // Determines if the place in (x,y) is occupied. 
-        // Optionally can take 'type' as a filter for obstackles
+        // Optionally can take 'ctype' as a filter for obstackles' collision group (not shape type)
         for (var i in ct.stack) {
-            if (ct.stack[i].shape.type && ct.stack[i] !== me && (ct.stack[i].ctype == type || !type)) {
-                if (ct.stack[i].shape.type + '.' + me.shape.type == 'rect.rect') {
-                    if (ct.place.check['rect.rect'](ct.stack[i].x - ct.stack[i].shape.left, ct.stack[i].y - ct.stack[i].shape.top,ct.stack[i].x + ct.stack[i].shape.right,ct.stack[i].y + ct.stack[i].shape.bottom,x - me.shape.left, y - me.shape.top,x + me.shape.right,y + me.shape.bottom))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'circle.circle') {
-                    if (ct.place.check['circle.circle'](ct.stack[i].x,ct.stack[i].y,ct.stack[i].shape.r,x,y,me.shape.r))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'circle.rect') {
-                    if (ct.place.check['circle.rect'](ct.stack[i].x,ct.stack[i].y,ct.stack[i].shape.r,x - me.shape.left, y - me.shape.top,x + me.shape.right,y + me.shape.bottom))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'rect.circle') {
-                    if (ct.place.check['circle.rect'](x,y,me.shape.r,ct.stack[i].x - ct.stack[i].shape.left, ct.stack[i].y - ct.stack[i].shape.top,ct.stack[i].x + ct.stack[i].shape.right,ct.stack[i].y + ct.stack[i].shape.bottom))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'circle.point') {
-                    if (ct.place.check['circle.point'](ct.stack[i].x,ct.stack[i].y,ct.stack[i].shape.r,x,y))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'point.circle') {
-                    if (ct.place.check['circle.point'](x,y,me.shape.r,ct.stack[i].x,ct.stack[i].y))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'point.point') {
-                    if (ct.stack[i].y == y && ct.stack[i].x == x)
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'point.rect') {
-                    if (ct.place.check['rect.point'](x - me.shape.left, y - me.shape.top,x + me.shape.right,y + me.shape.bottom,ct.stack[i].x,ct.stack[i].y))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'rect.point') {
-                    if (ct.place.check['rect.point'](ct.stack[i].x - ct.stack[i].shape.left, ct.stack[i].y - ct.stack[i].shape.top,ct.stack[i].x + ct.stack[i].shape.right,ct.stack[i].y + ct.stack[i].shape.bottom,x,y))
-                        return ct.stack[i];
+            if (ct.stack[i] !== me && (ct.stack[i].ctype === ctype || !ctype)) {
+                if (ct.place.collide(me, ct.stack[i])) {
+                    return ct.stack[i];
                 }
             }
         }
         return false;
     },
-    meet(me,x,y,type) {
+    free(me, x, y, ctype) {
+        return !ct.place.occupied(me, x, y, ctype);
+    },
+    meet(me, x, y, type) {
         // ct.place.meet(<me: Copy, x: Number, y: Number>[, type: Type])
         // detects collision between a given copy and a copy of a certain type
-        for (i in ct.stack) {
-            if (ct.stack[i].shape.type && ct.stack[i]!== me && ct.stack[i].type == type) {
-                if (ct.stack[i].shape.type + '.' + me.shape.type == 'rect.rect') {
-                    if (ct.place.check['rect.rect'](ct.stack[i].x - ct.stack[i].shape.left, ct.stack[i].y - ct.stack[i].shape.top,ct.stack[i].x + ct.stack[i].shape.right,ct.stack[i].y + ct.stack[i].shape.bottom,x - me.shape.left, y - me.shape.top,x + me.shape.right,y + me.shape.bottom))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'circle.circle') {
-                    if (ct.place.check['circle.circle'](ct.stack[i].x,ct.stack[i].y,ct.stack[i].shape.r,x,y,me.shape.r))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'circle.rect') {
-                    if (ct.place.check['circle.rect'](ct.stack[i].x,ct.stack[i].y,ct.stack[i].shape.r,x - me.shape.left, y - me.shape.top,x + me.shape.right,y + me.shape.bottom))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'rect.circle') {
-                    if (ct.place.check['circle.rect'](x,y,me.shape.r,ct.stack[i].x - ct.stack[i].shape.left, ct.stack[i].y - ct.stack[i].shape.top,ct.stack[i].x + ct.stack[i].shape.right,ct.stack[i].y + ct.stack[i].shape.bottom))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'circle.point') {
-                    if (ct.place.check['circle.point'](ct.stack[i].x,ct.stack[i].y,ct.stack[i].shape.r,x,y))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'point.circle') {
-                    if (ct.place.check['circle.point'](x,y,me.shape.r,ct.stack[i].x,ct.stack[i].y))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'point.point') {
-                    if (ct.stack[i].y == y && ct.stack[i].x == x)
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'point.rect') {
-                    if (ct.place.check['circle.point'](x - me.shape.left, y - me.shape.top,x + me.shape.right,y + me.shape.bottom,ct.stack[i].x,ct.stack[i].y))
-                        return ct.stack[i];
-                } else if (ct.stack[i].shape.type + '.' + me.shape.type == 'rect.point') {
-                    if (ct.place.check['circle.point'](ct.stack[i].x - ct.stack[i].shape.left, ct.stack[i].y - ct.stack[i].shape.top,ct.stack[i].x + ct.stack[i].shape.right,ct.stack[i].y + ct.stack[i].shape.bottom,x,y))
-                        return ct.stack[i];
-                }
+        var oldx = me.x, 
+            oldy = me.y;
+        me.x = x;
+        me.y = y; 
+        for (var i in ct.types.list[type]) {
+            if (ct.types.list[type][i] !== me && ct.place.collide(me, ct.types.list[type][i])) {
+                me.x = oldx;
+                me.y = oldy;
+                return ct.types.list[type][i];
             }
         }
+        me.x = oldx;
+        me.y = oldy;
         return false;
     },
     lastdist: null,
-    nearest(x,y,type) {
+    nearest(x, y, type) {
         // ct.place.nearest(<x: Number, y: Number, type: Type>)
         if (ct.types.list[type].length > 0) {
-                var dist = Math.sqrt(Math.abs((x-ct.types.list[type][0].y)*(y-ct.types.list[type][0].y)));
-                var inst = ct.types.list[type][0];
-                var i;
-                for (i in ct.types.list[type]) {
-                        if (Math.sqrt(Math.abs((x-ct.types.list[type][i].y)*(y-ct.types.list[type][i].y))) < dist) {
-                                dist = Math.sqrt(Math.abs((x-ct.types.list[type][i].y)*(y-ct.types.list[type][i].y)));
-                                inst = ct.types.list[type][i];
-                        }
+            var dist = Math.sqrt(Math.abs((x-ct.types.list[type][0].y)*(y-ct.types.list[type][0].y)));
+            var inst = ct.types.list[type][0];
+            var i;
+            for (i in ct.types.list[type]) {
+                if (Math.sqrt(Math.abs((x-ct.types.list[type][i].y)*(y-ct.types.list[type][i].y))) < dist) {
+                    dist = Math.sqrt(Math.abs((x-ct.types.list[type][i].y)*(y-ct.types.list[type][i].y)));
+                    inst = ct.types.list[type][i];
                 }
-                ct.place.lastdist = dist;
-                return inst;
-        } else
-                return false;
+            }
+            ct.place.lastdist = dist;
+            return inst;
+        }
+        return false;
     },
-    'furthest'(x, y, type) {
+    furthest(x, y, type) {
         // ct.place.furthest(<x: Number, y: Number, type: Type>)
         if (ct.types.list[type].length > 0) {
-                var dist = Math.sqrt(Math.abs((x-ct.types.list[type][0].y) * (y-ct.types.list[type][0].y)));
-                var inst = ct.types.list[type][0];
-                var i;
-                for (i in ct.types.list[type]) {
-                        if (Math.sqrt(Math.abs((x - ct.types.list[type][i].y) * (y-ct.types.list[type][i].y))) > dist) {
-                                dist = Math.sqrt(Math.abs((x - ct.types.list[type][i].y) * (y - ct.types.list[type][i].y)));
-                                inst = ct.types.list[type][i];
-                        }
+            var dist = Math.sqrt(Math.abs((x-ct.types.list[type][0].y) * (y-ct.types.list[type][0].y)));
+            var inst = ct.types.list[type][0];
+            var i;
+            for (i in ct.types.list[type]) {
+                if (Math.sqrt(Math.abs((x - ct.types.list[type][i].y) * (y-ct.types.list[type][i].y))) > dist) {
+                    dist = Math.sqrt(Math.abs((x - ct.types.list[type][i].y) * (y - ct.types.list[type][i].y)));
+                    inst = ct.types.list[type][i];
                 }
-                ct.place.lastdist = dist;
-                return inst;
-        } else
-                return false;
+            }
+            ct.place.lastdist = dist;
+            return inst;
+        }
+        return false;
     },
-    'go'(me, x, y, speed, type) {
+    go(me, x, y, speed, type) {
         // ct.place.go(<me: Copy, x: Number, y: Number, speed: Number>[, type: String])
         // tries to reach the target with simple obstackle avoidance algorithm
 
         // if we are too close to the destination, exit
-        if (ct.pdc(me.x, me.y, x, y)<speed) return;
+        if (ct.pdc(me.x, me.y, x, y) < speed) {
+            return;
+        }
         var dir = ct.pdn(me.x, me.y, x, y);
         if (type) {
             //if there are no obstackles in front of us, go forward
-            if (ct.place.free(me, me.x+ct.ldx(speed, dir), me.y+ct.ldy(speed, dir), type)) {
-                    me.x += ct.ldx(speed, dir);
-                    me.y += ct.ldy(speed, dir);
-                    me.dir = dir;
+            if (ct.place.free(me, me.x+ct.u.ldx(speed, dir), me.y+ct.u.ldy(speed, dir), type)) {
+                me.x += ct.u.ldx(speed, dir);
+                me.y += ct.u.ldy(speed, dir);
+                me.dir = dir;
             // otherwise, try to change direction by 30...60...90 degrees. 
             // Direction changes over time (ct.place.m).
-            } else if (ct.place.free(me, me.x+ct.ldx(speed, dir+30 * ct.place.m), me.y+ct.ldy(speed, dir+30 * ct.place.m), type)) {
-                    me.x += ct.ldx(speed, dir+30 * ct.place.m);
-                    me.y += ct.ldy(speed, dir+30 * ct.place.m);
-                    me.dir = dir+30 * ct.place.m;
-            } else if (ct.place.free(me, me.x+ct.ldx(speed, dir+60 * ct.place.m), me.y+ct.ldy(speed, dir+60 * ct.place.m), type)) {
-                    me.x += ct.ldx(speed, dir+60 * ct.place.m);
-                    me.y += ct.ldy(speed, dir+60 * ct.place.m);
-                    me.dir = dir+60 * ct.place.m;
-            } else if (ct.place.free(me, me.x+ct.ldx(speed, dir+90 * ct.place.m), me.y+ct.ldy(speed, dir+90 * ct.place.m), type)) {
-                    me.x += ct.ldx(speed, dir+90 * ct.place.m);
-                    me.y += ct.ldy(speed, dir+90 * ct.place.m);
-                    me.dir = dir+90 * ct.place.m;
+            } else if (ct.place.free(me, me.x+ct.u.ldx(speed, dir+30 * ct.place.m), me.y+ct.u.ldy(speed, dir+30 * ct.place.m), type)) {
+                me.x += ct.u.ldx(speed, dir+30 * ct.place.m);
+                me.y += ct.u.ldy(speed, dir+30 * ct.place.m);
+                me.dir = dir+30 * ct.place.m;
+            } else if (ct.place.free(me, me.x+ct.u.ldx(speed, dir+60 * ct.place.m), me.y+ct.u.ldy(speed, dir+60 * ct.place.m), type)) {
+                me.x += ct.u.ldx(speed, dir+60 * ct.place.m);
+                me.y += ct.u.ldy(speed, dir+60 * ct.place.m);
+                me.dir = dir+60 * ct.place.m;
+            } else if (ct.place.free(me, me.x+ct.u.ldx(speed, dir+90 * ct.place.m), me.y+ct.u.ldy(speed, dir+90 * ct.place.m), type)) {
+                me.x += ct.u.ldx(speed, dir+90 * ct.place.m);
+                me.y += ct.u.ldy(speed, dir+90 * ct.place.m);
+                me.dir = dir+90 * ct.place.m;
             }
-        } else {
-            if (ct.place.free(me, me.x+ct.ldx(speed, dir), me.y+ct.ldy(speed, dir))) {
-                    me.x += ct.ldx(speed, dir);
-                    me.y += ct.ldy(speed, dir);
-                    me.dir = dir;
-            } else if (ct.place.free(me, me.x+ct.ldx(speed, dir+30 * ct.place.m), me.y+ct.ldy(speed, dir+30 * ct.place.m))) {
-                    me.x += ct.ldx(speed, dir+30 * ct.place.m);
-                    me.y += ct.ldy(speed, dir+30 * ct.place.m);
-                    me.dir = dir+30 * ct.place.m;
-            } else if (ct.place.free(me, me.x+ct.ldx(speed, dir+60 * ct.place.m), me.y+ct.ldy(speed, dir+6 * ct.place.m))) {
-                    me.x += ct.ldx(speed, dir+60 * ct.place.m);
-                    me.y += ct.ldy(speed, dir+60 * ct.place.m);
-                    me.dir = dir+60 * ct.place.m;
-            } else if (ct.place.free(me, me.x+ct.ldx(speed, dir+90 * ct.place.m), me.y+ct.ldy(speed, dir+90 * ct.place.m))) {
-                    me.x += ct.ldx(speed, dir+90 * ct.place.m);
-                    me.y += ct.ldy(speed, dir+90 * ct.place.m);
-                    me.dir = dir+90 * ct.place.m;
-            }
+        } else if (ct.place.free(me, me.x+ct.u.ldx(speed, dir), me.y+ct.u.ldy(speed, dir))) {
+            me.x += ct.u.ldx(speed, dir);
+            me.y += ct.u.ldy(speed, dir);
+            me.dir = dir;
+        } else if (ct.place.free(me, me.x+ct.u.ldx(speed, dir+30 * ct.place.m), me.y+ct.u.ldy(speed, dir+30 * ct.place.m))) {
+            me.x += ct.u.ldx(speed, dir+30 * ct.place.m);
+            me.y += ct.u.ldy(speed, dir+30 * ct.place.m);
+            me.dir = dir+30 * ct.place.m;
+        } else if (ct.place.free(me, me.x+ct.u.ldx(speed, dir+60 * ct.place.m), me.y+ct.u.ldy(speed, dir+6 * ct.place.m))) {
+            me.x += ct.u.ldx(speed, dir+60 * ct.place.m);
+            me.y += ct.u.ldy(speed, dir+60 * ct.place.m);
+            me.dir = dir+60 * ct.place.m;
+        } else if (ct.place.free(me, me.x+ct.u.ldx(speed, dir+90 * ct.place.m), me.y+ct.u.ldy(speed, dir+90 * ct.place.m))) {
+            me.x += ct.u.ldx(speed, dir+90 * ct.place.m);
+            me.y += ct.u.ldy(speed, dir+90 * ct.place.m);
+            me.dir = dir+90 * ct.place.m;
         }
     }
 };
