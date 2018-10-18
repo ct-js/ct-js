469e009f08a6936a19e2918cedf6c7a23153e54d
Cosmo Myzrail Gorynych <admin@nersta.ru>
2018-10-18 23:27:36 +1200

    :alembic: EQS System

--- app/ct.libs/eqs/index.js
+++ app/ct.libs/eqs/index.js
@@ -34,52 +34,89 @@
             } else if (type === 'grid') {
                 const w = validate(options.w, 'w', 'number'),
                     h = validate(options.h, 'h', 'number'),
-                    gridSize = validate(options.size, 'size', 'number');
-                for (let ix = 0; ix < w; ix++) {
-                    for (let iy = 0; iy < h; iy++) {
-                        this.points.push({
-                            x: x + (ix - w / 2) * gridSize,
-                            y: y + (iy - h / 2) * gridSize,
+                    gridSizeX = validate(options.sizeX, 'sizeX', 'number'),
+                    gridSizeY = validate(options.sizeY, 'sizeY', 'number');
+                this.grid = [];
+                for (let iy = 0; iy < h; iy++) {
+                    const row = [];
+                    this.grid.push(row);
+                    for (let ix = 0; ix < w; ix++) {
+                        const point = {
+                            x: x + (ix - w / 2) * gridSizeX,
+                            y: y + (iy - h / 2) * gridSizeY,
                             score: 1
-                        });
+                        };
+                        if (options.triangle && iy % 2 === 1) {
+                            point.x += gridSizeX / 2;
+                        }
+                        this.points.push(point);
+                        row.push(point);
                     }
                 }
             } else if (type === 'circle') {
                 const r = validate(options.r, 'r', 'number'),
-                      gridSize = validate(options.size, 'size', 'number'),
-                      w = Math.floor(r*2 / gridSize);
+                      gridSizeX = validate(options.sizeX, 'sizeX', 'number'),
+                      gridSizeY = validate(options.sizeY, 'sizeY', 'number'),
+                      w = Math.floor(r*2 / gridSizeX),
+                      h = Math.floor(r*2 / gridSizeY);
                 for (let ix = 0; ix < w; ix++) {
-                    for (let iy = 0; iy < w; iy++) {
-                        const xx = (ix - w / 2) * gridSize,
-                              yy = (iy - w / 2) * gridSize;
+                    for (let iy = 0; iy < h; iy++) {
+                        let xx = (ix - w / 2) * gridSizeX;
+                        const yy = (iy - h / 2) * gridSizeY;
+                        if (options.triangle && iy % 2 === 1) {
+                            xx += gridSizeX / 2;
+                        }
                         if (Math.hypot(xx, yy) <= r) {
-                            this.points.push({
+                            const point = {
                                 x: x + xx,
                                 y: y + yy,
                                 score: 1
-                            });
+                            };
+                            this.points.push(point);
                         }
                     }
+                }
+            } else if (type === 'copies') {
+                if (!(validate(options, 'copyType', 'string') in ct.types.list)) {
+                    throw new Error(`[ct.eqs] The '${options.copyType}' type does not exist.`);
+                }
+                for (const copy of ct.types.list[options.copyType]) {
+                    if (options.limit && ct.u.pdc(copy.x, copy.y, options.x, options.y) > options.limit) {
+                        continue;
+                    }
+                    this.points.push({
+                        x: copy.x,
+                        y: copy.y,
+                        score: 1
+                    });
                 }
             } else {
                 throw new Error(`[ct.eqs] Unknown EQ type: \`${type}\``);
             }
             return this;
         }
-        concat(eq) {
-            return new EQSQuery(this.points.concat(eq.points));
-        }
+        /* Modifiers */
         score(func) {
             for (const point of this.points) {
                 func(point);
             }
             return this;
         }
+        scoreSpaced(func) {
+            if (!('grid' in this)) {
+                throw new Error('[ct.eqs] The scoreSpaced method may be only called on pure grids');
+            }
+            for (let iy = 0, ly = this.grid[iy].length; iy < ly; iy++) {
+                for (let ix = 0, lx = this.grid.length; ix < lx; ix++) {
+                   func(this.grid[iy][ix], this.grid, ix, iy);
+                }
+            }
+        }
         sort() {
             this.points.sort((a, b) => b.score - a.score);
             return this;
         }
-        normalize () {
+        normalize() {
             var max = this.points[0].score,
                 min = this.points[0].score;
             var i = 1,
@@ -99,30 +136,46 @@
             }
             return this;
         }
+        reverse() {
+            this.points.reverse();
+            return this;
+        }
+        portion(rate) {
+            this.points.length = Math.min(this.points.length, Math.max(1, Math.round(this.points.length * rate)));
+            delete this.grid;
+            return this;
+        }
+        /* Utilities */
+        draw() {
+            for (const point of this.points) {
+                ct.x.fillStyle = `rgb(${(1-point.score) * 255}, ${point.score * 255}, 0)`;
+                ct.draw.circle(point.x, point.y, 2);
+            }
+        }
+        concat(eq) {
+            return new EQSQuery(this.points.concat(eq.points));
+        }
+        /* Getters */
         getFirst(n) {
             if (n === void 0) {
                 return this.points[0];
             }
             return this.points.slice(0, n);
         }
+        getLast(n) {
+            if (n === void 0) {
+                return this.points[this.points - 1];
+            }
+            return this.points.slice(-n);
+        }
         getPortion(rate) {
             return this.points.slice(0, Math.round(rate * this.points.length));
-        }
-        portion(rate) {
-            this.points.length = Math.min(this.points.length, Math.max(1, Math.round(this.points.length * rate)));
-            return this;
         }
         getRandom() {
             return this.points[Math.floor(Math.random() * this.points.length)];
         }
         getBetterRandom() {
             return this.points[Math.floor(Math.random()*Math.random() * this.points.length)];
-        }
-        draw() {
-            for (const point of this.points) {
-                ct.x.fillStyle = `rgb(${(1-point.score) * 255}, ${point.score * 255}, 0)`;
-                ct.draw.circle(point.x, point.y, 2);
-            }
         }
     }
     
