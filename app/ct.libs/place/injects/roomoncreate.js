03c6d867962f0b15b0f0eac2f8952d44f640ed13
Cosmo Myzrail Gorynych <admin@nersta.ru>
2018-10-27 22:25:33 +1200

    :doughnut: ct.place little bugs and compatibility

--- app/ct.libs/place/injects/roomoncreate.js
+++ app/ct.libs/place/injects/roomoncreate.js
@@ -1,15 +1,18 @@
 ct.place.tileGrid = {};
-for (const layer of ct.types.list.TILELAYER) {
-    for (let i = 0, l = layer.tiles.length; i < l; i++) {
-        const t = layer.tiles[i];
-        t.$chashes = ct.place.getHashes(t);
-        for (const hash of t.$chashes) {
-            if (!(hash in ct.place.tileGrid)) {
-                ct.place.tileGrid[hash] = [t];
-            } else {
-                ct.place.tileGrid[hash].push(t);
+if (ct.types.list.TILELAYER) {
+    for (const layer of ct.types.list.TILELAYER) {
+        for (let i = 0, l = layer.tiles.length; i < l; i++) {
+            const t = layer.tiles[i];
+            t.$chashes = ct.place.getHashes(t);
+            /* eslint max-depth: 0 */
+            for (const hash of t.$chashes) {
+                if (!(hash in ct.place.tileGrid)) {
+                    ct.place.tileGrid[hash] = [t];
+                } else {
+                    ct.place.tileGrid[hash].push(t);
+                }
             }
+            t.depth = layer.depth;
         }
-        t.depth = layer.depth;
     }
 }
