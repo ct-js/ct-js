cb8ebf8a75b59de1b3fb21064e947723caf7c091
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-04-09 02:09:34 +1200

    :bug: Fix ct.sound.globalVolume method

--- app/data/ct.libs/sound.howler/index.js
+++ app/data/ct.libs/sound.howler/index.js
@@ -174,7 +174,7 @@ ct.sound.fade = function(name, newVolume, duration, id) {
  * 
  * @returns {Number} The current volume.
  */
-ct.sound.globalVolume = Howler.volume;
+ct.sound.globalVolume = Howler.volume.bind(Howler);
 
 ct.sound.exists = function(name) {
     return (name in ct.res.sounds);
