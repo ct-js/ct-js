797daff9a9570ca9693c54b699baacc3beefab4d
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-08-14 14:47:19 +1200

    :sparkles: Add a ct.cutscene module

---
+++ app/data/ct.libs/cutscene/README.md
@@ -0,0 +1,36 @@
+A module for showing cutscenes from youtube/vimeo or a direct link to a video.
+
+## `ct.cutscene.show(url)`
+
+Shows the cutscene and returns a reference to it.
+
+It can be a link to youtube or vimeo or a direct link to a video. These values are valid:
+
+* `https://youtu.be/xxxxxxxxxx`
+* `https://www.youtube.com/watch?v=xxxxxxxxxx`
+* `https://www.youtube.com/embed/xxxxxxxxxx`
+* `https://vimeo.com/xxxxxxxxxx`
+* `https://player.vimeo.com/video/xxxxxxxxxx`
+* `https://awesomegames.rock/trailers/catmintide.mp4`
+* `./myVideo.mp4` (if you put it inside your `project/include` folder)
+
+No extra settings or parameters for Vimeo or YouTube are needed, ct.cutscene will try to remove most of the visible controls from the video. **Note:** Vimeo videos will always have controls unless you have a premium account. Vimeo videos will also fail to load in the integrated debugger; please test it in your default browser.
+
+## `ct.cutscene.remove(cutscene)`
+
+Use this to manually remove the player. If the video ends, it will be removed automatically, but you probably want to provide controls to a player, e.g. Escape button to skip the cutscene.
+
+```js
+// Say, this is placed in a room's On Create event
+this.cutscene = ct.cutscene.show('https://www.youtube.com/watch?v=F8VzZe1FqEM');
+cutscene.then(() => {
+    this.cutscene = void 0;
+}).catch(error => {
+    console.error(error);
+});
+
+// In its Step event
+if (ct.actions.Exit.pressed && this.cutscene) {
+    ct.cutscene.remove(this.cutscene);
+}
+```
---
+++ app/data/ct.libs/cutscene/index.js
@@ -0,0 +1,127 @@
+(function () {
+    /*  Tests for https://youtu.be/xxxxxxxxxx
+                  https://www.youtube.com/watch?v=xxxxxxxxxx
+                  https://www.youtube.com/embed/xxxxxxxxxx */
+    const youtubeRegex = /youtu(\.be\/|be\.com\/(watch\?v=|embed\/))(\w+)/,
+    /*  Tests for https://vimeo.com/xxxxxxxxxx
+                  https://player.vimeo.com/video/xxxxxxxxxx */
+          vimeoRegex = /vimeo\.com\/(video\/)?(\d+)/;
+    ct.cutscene = {
+        remove(cutscene) {
+            if (cutscene.removed) {
+                throw new Error(`[ct.cutscene]: This cutscene (${cutscene.url}) has already been removed! Clean up any references to it to clear the memory.`);
+            }
+            if (cutscene.type === 'vimeo' || cutscene.type === 'youtube') {
+                cutscene.raw.destroy();
+            } else {
+                document.body.removeChild(cutscene.raw);
+            }
+            cutscene.removed = true;
+        },
+        show(url) {
+            var youtube = youtubeRegex.exec(url),
+                vimeo = vimeoRegex.exec(url),
+                cutscene = {
+                    url,
+                    raw: null
+                };
+            var promise = new Promise((resolve, reject) => {
+                if (youtube) {
+                    cutscene.type = 'youtube';
+                    const embed = 'https://www.youtube.com/embed/'+ youtube[3] + '?enablejsapi=1&controls=0&modestbranding=1&disablekb=1&rel=0&showinfo=0',
+                          iframe = document.createElement('iframe');
+                    iframe.className = 'youtube aFullscreenVideo';
+                    iframe.src = embed;
+                    //iframe.style.opacity = 0;
+                    document.body.appendChild(iframe);
+                    // See https://developers.google.com/youtube/player_parameters
+                    const tag = document.createElement('script');
+                    tag.src = 'https://www.youtube.com/player_api';
+                    const [firstScriptTag] = document.getElementsByTagName('script');
+                    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+                    let player;
+                    /* global YT */
+                    window.onYouTubePlayerAPIReady = function() {
+                        player = new YT.Player(iframe, {
+                            videoId: youtube[3],
+                            events: {
+                                onReady: e => {
+                                    e.target.playVideo();
+                                    iframe.style.opacity = 1;
+                                    console.log('onReady');
+                                },
+                                onStateChange: data => {
+                                    console.log(data, 'statechange');
+                                    if (data.data === YT.PlayerState.ENDED) {
+                                        ct.cutscene.remove(cutscene);
+                                        resolve();
+                                    }
+                                },
+                                onError: err => {
+                                    ct.cutscene.remove(cutscene);
+                                    reject(err);
+                                }
+                            }
+                        });
+                        cutscene.raw = player;
+                    };
+                } else if (vimeo) {
+                    cutscene.type = 'vimeo';
+                    const embed = 'https://player.vimeo.com/video/'+ vimeo[2] + '?autoplay=1&title=0&byline=0&portrait=0&controls=0dnt=1&fun=0&playsinline=1',
+                          iframe = document.createElement('iframe');
+                    iframe.className = 'vimeo aFullscreenVideo';
+                    iframe.src = embed;
+                    document.body.appendChild(iframe);
+                    const tag = document.createElement('script');
+                    tag.src = 'https://player.vimeo.com/api/player.js';
+                    document.body.appendChild(tag);
+                    /* global Vimeo */
+                    tag.onload = () => {
+                        setTimeout(() => {
+                            const player = new Vimeo.Player(iframe);
+                            cutscene.raw = player;
+                            player.setAutopause(false);
+                            player.play();
+                            player.on('ended', () => {
+                                ct.cutscene.remove(cutscene);
+                                resolve();
+                            });
+                            player.on('error', e => {
+                                ct.cutscene.remove(cutscene);
+                                reject(e);
+                            });
+                        }, 1000);
+                    };
+                } else {
+                    const video = document.createElement('video');
+                    cutscene.type = 'file';
+                    video.className = 'direct aFullscreenVideo'; /* надо написать CSS-класс, чтобы заполнить весь экран */
+                    video.addEventListener('canplaythrough', () => {
+                        document.body.appendChild(video);
+                    });
+                    video.addEventListener('ended', () => {
+                        ct.cutscene.remove(cutscene);
+                        resolve();
+                    });
+                    video.on('abort', e => {
+                        ct.cutscene.remove(cutscene);
+                        reject(e);
+                    });
+                    video.on('error', e => {
+                        ct.cutscene.remove(cutscene);
+                        reject(e);
+                    });
+
+                    video.controls = false;
+                    video.autoplay = true;
+                    video.src = url;
+                    video.load();
+                    cutscene.raw = video;
+                }
+            });
+            cutscene.then = promise.then.bind(promise);
+            cutscene.catch = promise.catch.bind(promise);
+            return cutscene;
+        }
+    };
+})();
