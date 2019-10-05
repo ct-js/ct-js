9a29e2b233b5b41153f4837d2fd3d659bd4ba21c
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-10-05 21:46:19 +1200

    :zap: Allow empty strings to be present in language files
    Closes #98

--- src/node_requires/i18n.js
+++ src/node_requires/i18n.js
@@ -1,14 +1,20 @@
 const fs = require('fs-extra');
-const {extend} = require('./objectUtils');
+const {extendValid} = require('./objectUtils');
 
 const vocDefault = fs.readJSONSync('./data/i18n/English.json');
 var i18n;
 
 const loadLanguage = lang => {
-    const voc = fs.readJSONSync(`./data/i18n/${lang}.json`);
+    var voc;
+    try {
+        voc = fs.readJSONSync(`./data/i18n/${lang}.json`);
+    } catch (e) {
+        console.error(`An error occured while reading the language file ${lang}.json.`);
+        throw e;
+    }
     // eslint-disable-next-line no-console
     console.debug(`Loaded a language file ${lang}.json`);
-    i18n.languageJSON = extend({}, extend(vocDefault, voc));
+    i18n.languageJSON = extendValid(vocDefault, voc);
     return i18n.languageJSON;
 };
 
