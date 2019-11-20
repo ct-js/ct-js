81cd32ae694d1abad7c3d78de4d8f23f73cd6dc0
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-11-21 11:27:21 +1200

    :bento: Typedefs for ct.flow

--- app/data/ct.libs/flow/index.js
+++ app/data/ct.libs/flow/index.js
@@ -6,9 +6,9 @@
          * Constructs and returns a gate — a new function that will execute a given one only when the gate is opened.
          * The gate can be opened and closed with `ourGate.open()`, `yourGate.close()` and `yourGate.toggle()` methods,
          * and called later with `yourGate()`.
-         * 
+         *
          * **Example:**
-         * 
+         *
          * ```js
          * var gate = ct.flow.gate(() => {
          *     console.log('It works!');
@@ -17,7 +17,7 @@
          * gate.open();
          * gate(); // Will now work
          * ```
-         * 
+         *
          * @param {Function} func The function to execute.
          * @param {Boolean} [opened] The initial state of the created gate (`true` for being opened, default is `false`).
          * @returns {Function} The constructed gate.
@@ -46,9 +46,9 @@
          * Creates and returns a new function that will execute a function `func` with a delay
          * and no more than once in the given period. The call to the original function is made
          * after the period has elapsed, after which the delay function may be triggered again.
-         * 
+         *
          * **Example:**
-         * 
+         *
          * ```js
          * var delayed1 = ct.flow.delay(() => {
          *     console.log('It works!');
@@ -56,7 +56,7 @@
          * for (var i = 0; i < 3; i++) {
          *     delayed1(); // The original function will work only once, because there are no pauses
          * }
-         * 
+         *
          * var delayed2 = ct.flow.delay(() => {
          *     console.log('It works!');
          * }, 300);
@@ -64,9 +64,9 @@
          * ct.u.wait(500)
          * .then(delayed2); // Will work twice
          * ```
-         * 
+         *
          * **Note:** the created function will stop working if the game switches to another room.
-         * 
+         *
          * @param {Function} func The function to postpone
          * @param {Number} ms The period to wait, in milliseconds
          * @returns {Function} The delayed function
@@ -91,9 +91,9 @@
          * Returns a new function that sets a timer to call the original function,
          * similar to `ct.flow.delay`. This function, however, will reset its timer on each call.
          * This construction is also known as a cumulative delay.
-         * 
+         *
          * **Example:**
-         * 
+         *
          * ```js
          * var delayed1 = ct.flow.retriggerableDelay(() => {
          *     console.log('It works!');
@@ -101,7 +101,7 @@
          * delayed1();
          * ct.u.wait(2000)
          * .then(delayed2); // Will work once in 5 seconds
-         * 
+         *
          * var delayed2 = ct.flow.retriggerableDelay(() => {
          *     console.log('It works!');
          * }, 3000);
@@ -109,9 +109,9 @@
          * ct.u.wait(4000)
          * .then(delayed2); // Will work twice in 7 seconds
          * ```
-         * 
+         *
          * **Note:** the created function will stop working if the game switches to another room.
-         * 
+         *
          * @param {Function} func The function to postpone
          * @param {Number} ms The period to wait, in milliseconds
          * @returns {Function} The delayed function
@@ -137,7 +137,7 @@
          * Similar to `ct.flow.delay`, this method will return a new function that will limit
          * the execution of the `func` to max once in `ms` period. It will call the function first
          * and then block the execution for `ms` time, though.
-         * 
+         *
          * @param {Function} func The function to limit
          * @param {Number} ms The period to wait, in milliseconds
          * @returns {Function} a new triggerable function
