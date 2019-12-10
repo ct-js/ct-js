5436797c95f50d9a2c62a77491d4528b025e7361
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-12-10 12:33:18 +1200

    :zap: Make the `Room` class visible to everything

--- app/data/ct.release/rooms.js
+++ app/data/ct.release/rooms.js
@@ -1,58 +1,58 @@
+class Room extends PIXI.Container {
+    constructor(template) {
+        super();
+        this.x = this.y = 0;
+        this.uid = 0;
+        this.follow = this.borderX = this.borderY = this.followShiftX = this.followShiftY = this.followDrift = 0;
+        this.tileLayers = [];
+        this.backgrounds = [];
+        if (!ct.room) {
+            ct.room = ct.rooms.current = this;
+        }
+        if (template) {
+            this.onCreate = template.onCreate;
+            this.onStep = template.onStep;
+            this.onDraw = template.onDraw;
+            this.onLeave = template.onLeave;
+            this.template = template;
+            this.name = template.name;
+            for (let i = 0, li = template.bgs.length; i < li; i++) {
+                const bg = new ct.types.Background(template.bgs[i].texture, null, template.bgs[i].depth, template.bgs[i].extends);
+                this.backgrounds.push(bg);
+                ct.stack.push(bg);
+                this.addChild(bg);
+            }
+            for (let i = 0, li = template.tiles.length; i < li; i++) {
+                const tl = ct.rooms.addTileLayer(template.tiles[i]);
+                this.tileLayers.push(tl);
+                this.addChild(tl);
+            }
+            for (let i = 0, li = template.objects.length; i < li; i++) {
+                ct.types.make(template.objects[i].type, template.objects[i].x, template.objects[i].y, {
+                    tx: template.objects[i].tx,
+                    ty: template.objects[i].ty
+                }, this);
+            }
+        }
+        return this;
+    }
+    get x () {
+        return -this.position.x;
+    }
+    set x (value) {
+        this.position.x = -value;
+        return value;
+    }
+    get y () {
+        return -this.position.y;
+    }
+    set y (value) {
+        this.position.y = -value;
+        return value;
+    }
+}
 (function () {
     /* global deadPool */
-    class Room extends PIXI.Container {
-        constructor(template) {
-            super();
-            this.x = this.y = 0;
-            this.uid = 0;
-            this.follow = this.borderX = this.borderY = this.followShiftX = this.followShiftY = this.followDrift = 0;
-            this.tileLayers = [];
-            this.backgrounds = [];
-            if (!ct.room) {
-                ct.room = ct.rooms.current = this;
-            }
-            if (template) {
-                this.onCreate = template.onCreate;
-                this.onStep = template.onStep;
-                this.onDraw = template.onDraw;
-                this.onLeave = template.onLeave;
-                this.template = template;
-                this.name = template.name;
-                for (let i = 0, li = template.bgs.length; i < li; i++) {
-                    const bg = new ct.types.Background(template.bgs[i].texture, null, template.bgs[i].depth, template.bgs[i].extends);
-                    this.backgrounds.push(bg);
-                    ct.stack.push(bg);
-                    this.addChild(bg);
-                }
-                for (let i = 0, li = template.tiles.length; i < li; i++) {
-                    const tl = ct.rooms.addTileLayer(template.tiles[i]);
-                    this.tileLayers.push(tl);
-                    this.addChild(tl);
-                }
-                for (let i = 0, li = template.objects.length; i < li; i++) {
-                    ct.types.make(template.objects[i].type, template.objects[i].x, template.objects[i].y, {
-                        tx: template.objects[i].tx,
-                        ty: template.objects[i].ty
-                    }, this);
-                }
-            }
-            return this;
-        }
-        get x () {
-            return -this.position.x;
-        }
-        set x (value) {
-            this.position.x = -value;
-            return value;
-        }
-        get y () {
-            return -this.position.y;
-        }
-        set y (value) {
-            this.position.y = -value;
-            return value;
-        }
-    }
     var nextRoom;
     /**
      * @namespace
@@ -99,7 +99,7 @@
                 ct.rooms.switching = true;
             } else {
                 console.error('[ct.rooms] The room "' + room + '" does not exist!');
-            }            
+            }
         },
         switching: false,
         /**
