632ffbb4b66cdfe5fcbd49661b3525b2ca8e4e87
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-04-09 12:05:27 -0700

    :zap: :sparkles: Timed timers!

--- app/data/ct.release/timer.js
+++ app/data/ct.release/timer.js
@@ -6,6 +6,65 @@
  * - Add support for ending timers after a certain amount of time
  */
 
+class CtTimer {
+    /**
+     * An object for holding a timer
+     * 
+     * @param {string} name The name of the timer
+     * @param {boolean} [uiDelta=false] If `true`, it will use `ct.deltaUi` for counting time. if `false`, it will use `ct.delta` for counting time.a
+     * @param {number} [startTime=0] The amout of time to start at, **in milliseconds**
+     */
+    constructor(name, uiDelta = false, startTime = 0) {
+        this._name = name;
+        this.uiDelta = uiDelta;
+        this._time = startTime / 1000 * 60;
+        this.time = this._time * 1000 / 60;
+    }
+
+    /**
+     * Updates the timer. **DONT CALL THIS UNLESS YOU KNOW WHAT YOU ARE DOING**
+     *
+     * @returns {void}
+     * @private
+     */
+    update() {
+        this._time += this.uiDelta ? ct.deltaUi : ct.delta;
+        this.time = this._time * 1000 / 60;
+    }
+}
+
+class CtTimedTimer extends CtTimer {
+    /**
+     * An object for holding a timed timer
+     *
+     * @param {string} name The name of the timer
+     * @param {number} length The length for the timed timer, **in milliseconds**
+     * @param {function} callback The function to call when the timer ends.
+     * @param {boolean} [uiDelta=false] If `true`, it will use `ct.deltaUi` for counting time. if `false`, it will use `ct.delta` for counting time.a
+     * @param {number} [startTime=0] The amout of time to start at, **in milliseconds**
+     */
+    constructor(name, length, callback, uiDelta = false, startTime = 0) {
+        super(name, uiDelta, startTime);
+        this.length = length; // / 1000;
+        this.callback = callback;
+        this.done = false;
+    }
+
+    /**
+     * Updates the timer. **DONT CALL THIS UNLESS YOU KNOW WHAT YOU ARE DOING**
+     *
+     * @returns {void}
+     * @private
+     */
+    update() {
+        super.update();
+        if (this.time >= this.length && this.done !== true) {
+            this.done = true;
+            this.callback();
+        }
+    }
+}
+
 (function () {
     /**
      * Timer utilities
@@ -13,20 +72,15 @@
      */
     ct.timer = {
         /**
-         * An object with timers. Counted in seconds.
-         * @type Object<Number>
+         * An object with timers.
+         * @type Object<CtTimer>
          */
         timers: {},
         /**
-         * **DONT USE THIS UNLESS YOU KNOW WHAT YOU ARE DOING**
-         * @private
+         * An object with timed timers.
+         * @type Object<CtTimedTimer>
          */
-        _timers: {},
-        /**
-         * **DONT USE THIS UNLESS YOU KNOW WHAT YOU ARE DOING**
-         * @private
-         */
-        _timersUi: {},
+        timedTimers: {},
         /**
          * **DONT USE THIS UNLESS YOU KNOW WHAT YOU ARE DOING**
          * @private
@@ -41,29 +95,33 @@
          * @returns {void}
          */
         addTimer(name, uiDelta = false, startTime = 0) {
-            const _startTime = startTime * 60; // Convert to ct.delta time
-            if (uiDelta) {
-                this._timersUi[name.toString()] = Number(_startTime);
-                this.timers[name.toString()] = Number(startTime);
-            } else {
-                this._timers[name.toString()] = Number(_startTime);
-                this.timers[name.toString()] = Number(startTime);
-            }
+            this.timers[name.toString()] = new CtTimer(name, uiDelta, startTime);
+        },
+        /**
+         * Adds a new timed timer with the given name
+         *
+         * @param {string} name The name of the timer
+         * @param {number} length The length for the timed timer, **in milliseconds**
+         * @param {function} callback The function to call when the timer ends.
+         * @param {boolean} [uiDelta=false] If `true`, it will use `ct.deltaUi` for counting time. if `false`, it will use `ct.delta` for counting time.a
+        * @param {number} [startTime=0] The amout of time to start at, **in milliseconds**
+         * @returns {void}
+         */
+        addTimedTimer(name, length, callback, uiDelta = false, startTime = 0) {
+            this.timedTimers[name.toString()] = new CtTimedTimer(name, length, callback, uiDelta, startTime);
         },
         /**
          * Removes the timer with the given name
          * 
-         * @param {string} name The name of the timer, which you used to access the timer from `ct.timer.timers`.
+         * @param {string} name The name of the timer, which you used to access the timer from `ct.timer.timers` or `ct.timer.timedTimers`.
          * @returns {void}
          */
         removeTimer(name) {
             try {
-                delete this._timers[name.toString()];
                 delete this.timers[name.toString()];
             } catch (e) {
                 try {
-                    delete this._timersUi[name.toString()];
-                    delete this.timers[name.toString()];
+                    delete this.timedTimers[name.toString()];
                 } catch (e2) {
                     console.warn(`[ct.timer] Timer '${name.toString()}' does not exist!`);
                 }
@@ -76,9 +134,10 @@
          * @private
          */
         updateTimers() {
-            this.timers = {};
+            //this.timers = {};
+            //this.timedTimers = {};
 
-            if (Object.keys(this._timers).length > 0) for (const timerName in this._timers) {
+            /*if (Object.keys(this._timers).length > 0) for (const timerName in this._timers) {
                 this._timers[timerName] += ct.delta;
                 this.timers[timerName] = this._timers[timerName];
             }
@@ -86,19 +145,28 @@
             if (Object.keys(this._timersUi).length > 0) for (const timerName in this._timersUi) {
                 this._timersUi[timerName] += ct.deltaUi;
                 this.timers[timerName] = this._timersUi[timerName];
-            }
+            }*/
 
             if (Object.keys(this._timersInternal).length > 0) for (const timerName in this._timersInternal) {
                 this._timersInternal[timerName] += ct.deltaUi;
-                this.timers[timerName] = this._timersInternal[timerName];
+            }
+
+            if (Object.keys(this.timers).length > 0) for (const timerName in this.timers) {
+                this.timers[timerName].update();
+            }
+
+            // Timed timers
+
+            if (Object.keys(this.timedTimers).length > 0) for (const timerName in this.timedTimers) {
+                this.timedTimers[timerName].update();
             }
 
             //var temp1 = this._timers;
             //this.timers = ct.u.ext(temp1, this._timersUi);
 
-            if (Object.keys(this.timers).length > 0) for (const timerName in this.timers) {
-                this.timers[timerName] /= 60; // Change the time to seconds
-            }
+            //if (Object.keys(this.timers).length > 0) for (const timerName in this.timers) {
+            //this.timers[timerName] /= 60; // Change the time to seconds
+            //}
         }
     };
 })();
