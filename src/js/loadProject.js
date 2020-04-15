4d482242a4d7390b269d2ebb6ff0840e499a1cc3
Cosmo Myzrail Gorynych <admin@nersta.ru>
2020-04-15 16:37:10 +1200

    :bug: Fix initial zoom level in ct.IDE. It also improves how default settings are set.

--- src/js/loadProject.js
+++ src/js/loadProject.js
@@ -1,7 +1,4 @@
 (function (window) {
-    // Set the correct zoom
-    if (Number(require('electron').webFrame.getZoomFactor()) !== Number(localStorage.editorZooming)) { require('electron').webFrame.setZoomFactor(Number(localStorage.editorZooming)); }
-
     window.migrationProcess = window.migrationProcess || [];
     window.applyMigrationCode = function (version) {
         const process = window.migrationProcess.find(process => process.version === version);
