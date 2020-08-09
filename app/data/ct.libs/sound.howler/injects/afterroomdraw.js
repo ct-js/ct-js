ea77e91ae35a6a329e0e469c3b8f394e8b5c05a1
Cosmo Myzrail Gorynych <admin@nersta.ru>
2020-08-09 12:56:21 +1200

    :bento: Update Howler.js to v2.2.0

--- app/data/ct.libs/sound.howler/injects/afterroomdraw.js
+++ app/data/ct.libs/sound.howler/injects/afterroomdraw.js
@@ -1,5 +1,5 @@
 if (ct.sound.follow && !ct.sound.follow.kill) {
-    ct.sound.howler.pos(ct.sound.follow.x, ct.sound.follow.y, ct.sound.useDepth? ct.sound.follow.z : 0);
+    ct.sound.howler.pos(ct.sound.follow.x, ct.sound.follow.y, ct.sound.useDepth ? ct.sound.follow.z : 0);
 } else if (ct.sound.manageListenerPosition) {
-    ct.sound.howler.pos(ct.camera.x, ct.camera.y, ct.camera.z);
+    ct.sound.howler.pos(ct.camera.x, ct.camera.y, ct.camera.z || 0);
 }
