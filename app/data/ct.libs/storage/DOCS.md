38e0f83d3d39665bb163b29ea564f432a2235247
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-22 10:40:15 -0700

    :sparkles: Add storage catmod

---
+++ app/data/ct.libs/storage/DOCS.md
@@ -0,0 +1,15 @@
+## `ct.storage.set(name: String, value: any)`
+
+Sets a value to `localStorage`. Supports objects, arrays, numbers, strings, probably other types.
+
+## `ct.storage.get(name: String)`
+
+Gets a value from `localStorage`. Supports objects, arrays, numbers, strings, probably other types.
+
+## `ct.storage.setSession(name: String, value: any)`
+
+Sets a value to `sessionStorage`. Supports objects, arrays, numbers, strings, probably other types.
+
+## `ct.storage.getSession(name: String)`
+
+Gets a value from `sessionStorage`. Supports objects, arrays, numbers, strings, probably other types.
---
+++ app/data/ct.libs/storage/index.js
@@ -0,0 +1,31 @@
+(() => {
+    ct.storage = {
+        set(name, value) {
+            localStorage[name] = JSON.stringify(value) || value.toString();
+        },
+
+        get(name) {
+            let value;
+            try {
+                value = JSON.parse(localStorage[name]);
+            } catch (e) {
+                value = localStorage[name].toString();
+            }
+            return value;
+        },
+
+        setSession(name, value) {
+            sessionStorage[name] = JSON.stringify(value) || value.toString();
+        },
+
+        getSession(name) {
+            let value;
+            try {
+                value = JSON.parse(sessionStorage[name]);
+            } catch (e) {
+                value = sessionStorage[name].toString();
+            }
+            return value;
+        }
+    };
+})();
