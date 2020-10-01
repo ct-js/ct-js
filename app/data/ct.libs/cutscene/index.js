ce1fcb7e94428dbc21a8d8627e0e69ed79aa5ad5
Cosmo Myzrail Gorynych <admin@nersta.ru>
2020-10-01 14:33:51 +1200

    :sparkles: Specify multiple video formats at ct.cutscene

--- app/data/ct.libs/cutscene/index.js
+++ app/data/ct.libs/cutscene/index.js
@@ -1,4 +1,5 @@
-(function () {
+/* eslint-disable max-lines-per-function */
+(function ctCutscene() {
     /*  Tests for https://youtu.be/xxxxxxxxxx
                   https://www.youtube.com/watch?v=xxxxxxxxxx
                   https://www.youtube.com/embed/xxxxxxxxxx */
@@ -6,6 +7,118 @@
     /*  Tests for https://vimeo.com/xxxxxxxxxx
                   https://player.vimeo.com/video/xxxxxxxxxx */
           vimeoRegex = /vimeo\.com\/(video\/)?(\d+)/;
+
+    const playYoutube = function playYoutube(cutscene) {
+        return new Promise((resolve, reject) => {
+            cutscene.type = 'youtube';
+            const parsed = youtubeRegex.exec(cutscene.url);
+            const embed = 'https://www.youtube.com/embed/' +
+                           parsed[3] +
+                           '?enablejsapi=1&controls=0&modestbranding=1&disablekb=1&rel=0&showinfo=0',
+                  iframe = document.createElement('iframe');
+            iframe.className = 'youtube aFullscreenVideo';
+            iframe.src = embed;
+            //iframe.style.opacity = 0;
+            document.body.appendChild(iframe);
+            // See https://developers.google.com/youtube/player_parameters
+            const youtubeSdk = document.createElement('script');
+            youtubeSdk.src = 'https://www.youtube.com/player_api';
+            const [firstScriptTag] = document.getElementsByTagName('script');
+            firstScriptTag.parentNode.insertBefore(youtubeSdk, firstScriptTag);
+            let player;
+            /* global YT */
+            window.onYouTubePlayerAPIReady = function onYouTubePlayerAPIReady() {
+                player = new YT.Player(iframe, {
+                    videoId: parsed[3],
+                    events: {
+                        onReady: e => {
+                            e.target.playVideo();
+                            iframe.style.opacity = 1;
+                        },
+                        onStateChange: e => {
+                            if (e.data === YT.PlayerState.ENDED) {
+                                ct.cutscene.remove(cutscene);
+                                resolve();
+                            }
+                        },
+                        onError: err => {
+                            ct.cutscene.remove(cutscene);
+                            reject(err);
+                        }
+                    }
+                });
+                cutscene.raw = player;
+            };
+        });
+    };
+
+    const playVimeo = function playVimeo(cutscene) {
+        const vimeo = vimeoRegex.exec(cutscene.url);
+        return new Promise((resolve, reject) => {
+            cutscene.type = 'vimeo';
+            const embed = 'https://player.vimeo.com/video/' + vimeo[2] + '?autoplay=1&title=0&byline=0&portrait=0&controls=0dnt=1&fun=0&playsinline=1',
+                  iframe = document.createElement('iframe');
+            iframe.className = 'vimeo aFullscreenVideo';
+            iframe.src = embed;
+            document.body.appendChild(iframe);
+            const vimeoSdk = document.createElement('script');
+            vimeoSdk.src = 'https://player.vimeo.com/api/player.js';
+            document.body.appendChild(vimeoSdk);
+            /* global Vimeo */
+            vimeoSdk.onload = () => {
+                setTimeout(() => {
+                    const player = new Vimeo.Player(iframe);
+                    cutscene.raw = player;
+                    player.setAutopause(false);
+                    player.play();
+                    player.on('ended', () => {
+                        ct.cutscene.remove(cutscene);
+                        resolve();
+                    });
+                    player.on('error', e => {
+                        ct.cutscene.remove(cutscene);
+                        reject(e);
+                    });
+                }, 0);
+            };
+        });
+    };
+
+    const playVideo = function playVideo(cutscene, formats) {
+        return new Promise((resolve, reject) => {
+            const video = document.createElement('video');
+            cutscene.type = 'file';
+            video.className = 'direct aFullscreenVideo'; /* надо написать CSS-класс, чтобы заполнить весь экран */
+            video.addEventListener('canplaythrough', () => {
+                document.body.appendChild(video);
+            });
+            video.addEventListener('ended', () => {
+                ct.cutscene.remove(cutscene);
+                resolve();
+            });
+            video.addEventListener('abort', e => {
+                ct.cutscene.remove(cutscene);
+                reject(e);
+            });
+            video.addEventListener('error', e => {
+                ct.cutscene.remove(cutscene);
+                reject(e);
+            });
+
+            video.controls = false;
+            video.autoplay = true;
+            for (const format in formats) {
+                const source = document.createElement('source');
+                video.appendChild(source);
+                source.src = formats[format];
+                source.type = `video/${format}`;
+            }
+            document.body.appendChild(video);
+            video.load();
+            cutscene.raw = video;
+        });
+    };
+
     ct.cutscene = {
         remove(cutscene) {
             if (cutscene.removed) {
@@ -19,106 +132,27 @@
             cutscene.removed = true;
         },
         show(url) {
-            var youtube = youtubeRegex.exec(url),
-                vimeo = vimeoRegex.exec(url),
+            var promise,
                 cutscene = {
                     url,
                     raw: null
                 };
-            var promise = new Promise((resolve, reject) => {
+            if (typeof url === 'string') {
+                var youtube = youtubeRegex.test(url),
+                    vimeo = vimeoRegex.test(url);
                 if (youtube) {
-                    cutscene.type = 'youtube';
-                    const embed = 'https://www.youtube.com/embed/'+ youtube[3] + '?enablejsapi=1&controls=0&modestbranding=1&disablekb=1&rel=0&showinfo=0',
-                          iframe = document.createElement('iframe');
-                    iframe.className = 'youtube aFullscreenVideo';
-                    iframe.src = embed;
-                    //iframe.style.opacity = 0;
-                    document.body.appendChild(iframe);
-                    // See https://developers.google.com/youtube/player_parameters
-                    const tag = document.createElement('script');
-                    tag.src = 'https://www.youtube.com/player_api';
-                    const [firstScriptTag] = document.getElementsByTagName('script');
-                    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-                    let player;
-                    /* global YT */
-                    window.onYouTubePlayerAPIReady = function() {
-                        player = new YT.Player(iframe, {
-                            videoId: youtube[3],
-                            events: {
-                                onReady: e => {
-                                    e.target.playVideo();
-                                    iframe.style.opacity = 1;
-                                    console.log('onReady');
-                                },
-                                onStateChange: data => {
-                                    console.log(data, 'statechange');
-                                    if (data.data === YT.PlayerState.ENDED) {
-                                        ct.cutscene.remove(cutscene);
-                                        resolve();
-                                    }
-                                },
-                                onError: err => {
-                                    ct.cutscene.remove(cutscene);
-                                    reject(err);
-                                }
-                            }
-                        });
-                        cutscene.raw = player;
-                    };
+                    promise = playYoutube(cutscene);
                 } else if (vimeo) {
-                    cutscene.type = 'vimeo';
-                    const embed = 'https://player.vimeo.com/video/'+ vimeo[2] + '?autoplay=1&title=0&byline=0&portrait=0&controls=0dnt=1&fun=0&playsinline=1',
-                          iframe = document.createElement('iframe');
-                    iframe.className = 'vimeo aFullscreenVideo';
-                    iframe.src = embed;
-                    document.body.appendChild(iframe);
-                    const tag = document.createElement('script');
-                    tag.src = 'https://player.vimeo.com/api/player.js';
-                    document.body.appendChild(tag);
-                    /* global Vimeo */
-                    tag.onload = () => {
-                        setTimeout(() => {
-                            const player = new Vimeo.Player(iframe);
-                            cutscene.raw = player;
-                            player.setAutopause(false);
-                            player.play();
-                            player.on('ended', () => {
-                                ct.cutscene.remove(cutscene);
-                                resolve();
-                            });
-                            player.on('error', e => {
-                                ct.cutscene.remove(cutscene);
-                                reject(e);
-                            });
-                        }, 1000);
-                    };
+                    promise = playVimeo(cutscene);
                 } else {
-                    const video = document.createElement('video');
-                    cutscene.type = 'file';
-                    video.className = 'direct aFullscreenVideo'; /* надо написать CSS-класс, чтобы заполнить весь экран */
-                    video.addEventListener('canplaythrough', () => {
-                        document.body.appendChild(video);
-                    });
-                    video.addEventListener('ended', () => {
-                        ct.cutscene.remove(cutscene);
-                        resolve();
-                    });
-                    video.on('abort', e => {
-                        ct.cutscene.remove(cutscene);
-                        reject(e);
-                    });
-                    video.on('error', e => {
-                        ct.cutscene.remove(cutscene);
-                        reject(e);
-                    });
-
-                    video.controls = false;
-                    video.autoplay = true;
-                    video.src = url;
-                    video.load();
-                    cutscene.raw = video;
+                    const formats = {};
+                    formats[url.split('.').pop()] = url;
+                    promise = playVideo(cutscene, formats);
                 }
-            });
+            } else {
+                // Assume an object with format: url values
+                promise = playVideo(cutscene, url);
+            }
             cutscene.then = promise.then.bind(promise);
             cutscene.catch = promise.catch.bind(promise);
             cutscene.finally = promise.finally.bind(promise);
