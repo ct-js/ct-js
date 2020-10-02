fc862ab50a9ddc5bfeed8d866a5328f015191282
Cosmo Myzrail Gorynych <admin@nersta.ru>
2020-10-02 16:14:47 +1200

    :sparkles: Hot-reload for theme files

--- src/js/gulpWatch.js
+++ src/js/gulpWatch.js
@@ -14,7 +14,16 @@
                 }
             }
         };
-        gulp.watch(['./data/theme*.css', './index.html', './data/bundle.js', './data/js/**.js', './data/node_requires/**/*.js'], reload);
+        gulp.watch(['./index.html', './data/bundle.js', './data/js/**.js', './data/node_requires/**/*.js'], reload);
+
+        const refreshCss = cb => {
+            for (const link of document.querySelectorAll('link[rel=stylesheet]')) {
+                link.href = link.href.replace(/\?.*|$/, '?' + Date.now());
+            }
+            window.alertify.success('Updated CSS ✅');
+            cb();
+        };
+        gulp.watch(['./data/theme*.css'], refreshCss);
     } catch (e) {
         void 0;
     }
