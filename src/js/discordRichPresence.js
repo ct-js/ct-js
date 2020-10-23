88399169caf3a12f0406a765d11a915ff43a2dae
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-10-23 09:32:36 -0700

    :sparkles: :zap: Major rich presence improvements

--- src/js/discordRichPresence.js
+++ src/js/discordRichPresence.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 (function discordRichPresence() {
     const appId = '749670101904785502';
     const startTimestamp = new Date();
     let available = false;
+    let failed = false;
+    let startScreen = true;
 
     const RPC = require('discord-rpc');
     const client = new RPC.Client({
@@ -23,58 +26,109 @@
         return delay;
     };
 
+    const notifyFailed = () => {
+        if (!failed) {
+            failed = true;
+            setTimeout(() => {
+                // eslint-disable-next-line no-underscore-dangle
+                const {delay} = alertify._$$alertify;
+                alertify.delay(1000 * 10);
+                alertify.error('Running discord rich presence failed. If you are running discord, please tell us in the discord server.');
+                alertify.delay(delay);
+            }, 1500);
+        }
+    };
+
     const setActivity = limitRate(function setActivity(activity) {
+        console.debug('Activity:');
+        console.debug(activity);
         if (!available) {
+            console.warn('Didn\'t change discord rich presence because it\'s not available yet.');
             return;
         }
-        client.setActivity(activity)
-        .then(console.log)
-        .catch(console.error);
-    }, 1000 * 15);
+        client
+            .setActivity(activity)
+            .then(() => console.debug('Successfully changed rich presence.'))
+            .catch((...err) => {
+                console.error(...err);
+                notifyFailed();
+            });
+    }, 1000 * 2);
 
-    client.login({
-        clientId: appId
-    })
-    .catch(console.error);
+    const activityMap = {
+        debug: 'Testing a game',
+        project: 'Configuring a project',
+        textures: 'Working with textures',
+        ui: 'Editing fonts and styles',
+        fx: 'Creating particle systems',
+        sounds: 'Managing sounds',
+        types: 'Coding types',
+        rooms: 'Designing rooms'
+    };
+
+    const changeDiscordStatus = (tab) => {
+        let details = window.currentProject ? window.currentProject.settings.authoring.title : null;
+        let state = activityMap[tab];
+        if (!state || startScreen) {
+            state = 'Start screen';
+            details = void 0;
+        }
+        const activity = {
+            startTimestamp,
+            details,
+            state,
+            largeImageKey: 'icon-lg',
+            largeImageText: 'ct.js (https://ctjs.rocks/)',
+            smallImageKey: `icon-small-${tab}`,
+            smallImageText: state,
+            instance: false
+        };
+        setActivity(activity);
+    };
+
+    window.signals.on('globalTabChanged', (tab) => {
+        changeDiscordStatus(tab);
+    });
+
+    window.signals.on('projectLoaded', () => {
+        startScreen = false;
+        // eslint-disable-next-line no-underscore-dangle
+        setTimeout(() => changeDiscordStatus(document.querySelector('main-menu')._tag.tab), 250);
+    });
+
+
+    // Make sure we're always updated
+    setInterval(() => {
+        if (!startScreen) {
+            // eslint-disable-next-line no-underscore-dangle
+            changeDiscordStatus(document.querySelector('main-menu')._tag.tab);
+        } else {
+            changeDiscordStatus();
+        }
+    }, 1000 * 5);
+
+    window.signals.on('resetAll', () => {
+        startScreen = true;
+        changeDiscordStatus();
+    });
+
+    client
+        .login({
+            clientId: appId
+        })
+        .catch((...err) => {
+            console.error(...err);
+            notifyFailed();
+        });
     client.on('ready', () => {
         available = true;
+        console.debug('Ready to use discord rich presence!');
         setActivity({
             startTimestamp,
             details: 'Just launched',
             largeImageKey: 'icon-lg',
+            largeImageText: 'ct.js (https://ctjs.rocks/)',
             instance: false
         });
-    });
-
-    /**
-     * {
-            details: 'Testing',
-            state: 'Rich Presence',
-            smartTimestamp: new Date(),
-            largeImageText: 'Hello world',
-            smallImageText: 'Hello smaller world'
-        }
-     */
-    const activityMap = {
-        debug: 'Tests a game',
-        project: 'Configures project\'s settings',
-        textures: 'Works with textures',
-        ui: 'Edits fonts and styles',
-        fx: 'Creates particle systems',
-        sounds: 'Manages sounds',
-        types: 'Codes types',
-        rooms: 'Designs levels'
-    };
-    window.signals.on('globalTabChanged', tab => {
-        const activity = {
-            startTimestamp,
-            details: window.currentProject.settings.authoring.title || void 0,
-            state: activityMap[tab],
-            largeImageKey: 'icon-lg',
-            smallImageKey: `icon-small-${tab}`,
-            smallImageText: activityMap[tab],
-            instance: false
-        };
-        setActivity(activity);
     });
 })();
