f4d7adbf4a85cd089f1c91850ef5b28360130b00
Cosmo Myzrail Gorynych <admin@nersta.ru>
2021-04-09 19:16:36 +1200

    :bug: Fix page name and app color for exported games

--- src/node_requires/exporter/html.js
+++ src/node_requires/exporter/html.js
@@ -1,8 +1,8 @@
 const substituteHtmlVars = (str, project, injects) =>
     str.replace('<!-- %htmltop% -->', () => injects.htmltop)
         .replace('<!-- %htmlbottom% -->', () => injects.htmlbottom)
-        .replace(/<!-- %gametitle% -->/g, () => project.settings.title || 'ct.js game')
-        .replace(/<!-- %accent% -->/g, () => project.settings.accent || 'ct.js game')
+        .replace(/<!-- %gametitle% -->/g, () => project.settings.authoring.title || 'ct.js game')
+        .replace(/<!-- %accent% -->/g, () => project.settings.branding.accent || '#446adb')
         .replace('<!-- %particleEmitters% -->', () => ((project.emitterTandems && project.emitterTandems.length) ?
             '<script src="pixi-particles.min.js"></script>' :
             ''))
