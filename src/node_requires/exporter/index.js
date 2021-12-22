6e7204f3cc0e577a31d1044a9ad4d1a117e09466
Cosmo Myzrail Gorynych <admin@nersta.ru>
2021-12-22 18:40:54 +1200

    :zap: Update to pixi v6.2.1. This drops support for legacy renderers.

--- src/node_requires/exporter/index.js
+++ src/node_requires/exporter/index.js
@@ -188,13 +188,8 @@ const exportCtProject = async (project, projdir, production) => {
     const injections = await getInjections();
 
     /* Pixi.js */
-    if (settings.rendering.usePixiLegacy) {
-        await fs.copyFile(basePath + 'ct.release/pixi-legacy.min.js', path.join(writeDir, '/pixi.min.js'));
-        await fs.copyFile(basePath + 'ct.release/pixi-legacy.min.js.map', path.join(writeDir, '/pixi-legacy.min.js.map'));
-    } else {
-        await fs.copyFile(basePath + 'ct.release/pixi.min.js', path.join(writeDir, '/pixi.min.js'));
-        await fs.copyFile(basePath + 'ct.release/pixi.min.js.map', path.join(writeDir, '/pixi.min.js.map'));
-    }
+    await fs.copyFile(basePath + 'ct.release/pixi.min.js', path.join(writeDir, '/pixi.min.js'));
+    await fs.copyFile(basePath + 'ct.release/pixi.min.js.map', path.join(writeDir, '/pixi.min.js.map'));
     if (project.emitterTandems && project.emitterTandems.length) {
         await fs.copyFile(basePath + 'ct.release/pixi-particles.min.js', path.join(writeDir, '/pixi-particles.min.js'));
     }
