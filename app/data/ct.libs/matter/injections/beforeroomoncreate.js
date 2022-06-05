247321037bbe2633b0d2f56a8c75e82c913f8a23
Cosmo Myzrail Gorynych <admin@nersta.ru>
2022-06-05 17:53:52 +1200

    :bug: Update ct.matter: additional fields show only when physics are enabled for a template; fix physics breaking when additional rooms are appended to the current one

--- app/data/ct.libs/matter/injections/beforeroomoncreate.js
+++ app/data/ct.libs/matter/injections/beforeroomoncreate.js
@@ -1,4 +1,6 @@
-ct.room.matterEngine = Matter.Engine.create();
-ct.room.matterWorld = ct.room.matterEngine.world;
-ct.room.matterGravity = ct.room.matterGravity || [0, 9.8];
-[ct.room.matterWorld.gravity.x, ct.room.matterWorld.gravity.y] = ct.room.matterGravity;
+if (this === ct.room) {
+    ct.room.matterEngine = Matter.Engine.create();
+    ct.room.matterWorld = ct.room.matterEngine.world;
+    ct.room.matterGravity = ct.room.matterGravity || [0, 9.8];
+    [ct.room.matterWorld.gravity.x, ct.room.matterWorld.gravity.y] = ct.room.matterGravity;
+}
