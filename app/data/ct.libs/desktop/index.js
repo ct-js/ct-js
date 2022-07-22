adee65df7c4d913f1f3f8f6a97c7db340c37e5ec
Ehan <ehanahamed@ehan.dev>
2022-07-22 17:55:55 -0400

    ⚡ Desktop Catmod: New Function to Open DevTools

--- app/data/ct.libs/desktop/index.js
+++ app/data/ct.libs/desktop/index.js
@@ -1,6 +1,21 @@
 ct.desktop = {
     isNw: window.nw && window.nw.App,
     isElectron: null,
+    openDevTools() {
+        if (ct.desktop.isNw) {
+            if (window.iAmInCtIdeDebugger) {
+                // eslint-disable-next-line no-console
+                console.warn('We can\'t open the debugger because it should already be open in ct.js! Let\'s imagine it opened! :D');
+            } else {
+                nw.win.showDevTools();
+            }
+        } else if (ct.desktop.isElectron) {
+            require('electron').remote.getCurrentWindow().webContents.openDevTools();
+        } else {
+            //eslint-disable-next-line no-console
+            console.error('[ct.desktop/openDevTools] Unknown environment :c Are we in a browser?');
+        }
+    },
     quit() {
         if (ct.desktop.isNw) {
             if (window.iAmInCtIdeDebugger) {
