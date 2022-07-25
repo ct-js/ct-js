771515cede224ccbfaccd67c3225049cc9cc9297
Ehan <ehanahamed@ehan.dev>
2022-07-25 10:58:44 -0400

    ⚡ Desktop Catmod: Better Error Messages

--- app/data/ct.libs/desktop/index.js
+++ app/data/ct.libs/desktop/index.js
@@ -5,7 +5,7 @@ ct.desktop = {
         if (ct.desktop.isNw) {
             if (window.iAmInCtIdeDebugger) {
                 // eslint-disable-next-line no-console
-                console.warn('We can\'t open the debugger because it should be open in ct.js\'s editor already! Let\'s imagine it opened just now! :D');
+                console.warn('We can\'t open the debugger because it should be open in ct.js\'s editor already! Let\'s imagine it opened just now like it would in a desktop export! :D');
             } else {
                 const win = nw.Window.get();
                 win.showDevTools();
@@ -15,14 +15,14 @@ ct.desktop = {
             ipcRenderer.invoke('ct.desktop.openDevTools', options);
         } else {
             //eslint-disable-next-line no-console
-            console.error('[ct.desktop/openDevTools] Unknown environment :c Are we in a browser?');
+            console.error('[ct.desktop.openDevTools] Unknown environment :c Are we in a browser?');
         }
     },
     closeDevTools() {
         if (ct.desktop.isNw) {
             if (window.iAmInCtIdeDebugger) {
                 // eslint-disable-next-line no-console
-                console.warn('We can\'t close the debugger because this is running inside of ct.js\'s editor! Let\'s imagine it actually did close! :D');
+                console.warn('We can\'t close the debugger because this is running inside of ct.js\'s editor! Let\'s imagine it actually did close like it would in a desktop export! :D');
             } else {
                 const win = nw.Window.get();
                 win.closeDevTools();
@@ -32,14 +32,14 @@ ct.desktop = {
             ipcRenderer.invoke('ct.desktop.closeDevTools');
         } else {
             //eslint-disable-next-line no-console
-            console.error('[ct.desktop/closeDevTools] Unknown environment :c Are we in a browser?');
+            console.error('[ct.desktop.closeDevTools] Unknown environment :c Are we in a browser?');
         }
     },
     quit() {
         if (ct.desktop.isNw) {
             if (window.iAmInCtIdeDebugger) {
                 // eslint-disable-next-line no-console
-                console.warn('We can\'t quit because ct.js\'s editor would close as well. Let\'s imagine that the game has exited! :D');
+                console.warn('We can\'t quit because ct.js\'s editor would close as well. Let\'s imagine that the game has exited like it would in a desktop export! :D');
             } else {
                 nw.App.quit();
             }
@@ -47,7 +47,7 @@ ct.desktop = {
             const {ipcRenderer} = require('electron');
             ipcRenderer.invoke('ct.desktop.quit');
         } else {
-            console.error('[ct.desktop/quit] Unknown environment :c Are we in a browser?');
+            console.error('[ct.desktop.quit] Unknown environment :c Are we in a browser?');
         }
     }
 };
