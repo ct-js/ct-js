492d6ccc8cb6bde9c92b8d52d6de457c9b0af00d
Cosmo Myzrail Gorynych <admin@nersta.ru>
2022-08-21 19:46:49 +1200

    :zap: Move migration script for room editor from v2.1.0 to v2.2.0.

--- src/js/projectMigrationScripts/2.1.0.js
+++ src/js/projectMigrationScripts/2.1.0.js
@@ -37,7 +37,6 @@ window.migrationProcess.push({
             }
         }
         for (const room of project.rooms) {
-            room.simulate = true;
             if (!room.events) {
                 room.events = [];
                 for (const key in roomEventMap) {
@@ -53,40 +52,6 @@ window.migrationProcess.push({
                 }
             }
             room.type = 'room';
-        }
-        // Break up tile chunks into individual tiles
-        for (const room of project.rooms) {
-            for (const layer of room.tiles) {
-                layer.tiles = layer.tiles.reduce((tiles, tile) => {
-                    const tex = project.textures.find(t => t.uid === tile.texture);
-                    if (!tile.grid) {
-                        tiles.push(tile);
-                        return tiles;
-                    }
-                    // grid: [tileX, tileY, tileSpanX, tileSpanY]
-                    // eslint-disable-next-line prefer-destructuring
-                    for (let x = tile.grid[0]; x < tile.grid[0] + tile.grid[2]; x++) {
-                        // eslint-disable-next-line prefer-destructuring
-                        for (let y = tile.grid[1]; y < tile.grid[1] + tile.grid[3]; y++) {
-                            tiles.push({
-                                x: tile.x + tex.axis[0],
-                                y: tile.y + tex.axis[1],
-                                opacity: tile.opacity ?? 1,
-                                tint: tile.tint ?? 0xffffff,
-                                scale: {
-                                    x: tile.scale?.x ?? 1,
-                                    y: tile.scale?.y ?? 1
-                                },
-                                frame: x + y * tex.grid[0],
-                                rotation: tile.rotation ?? 0,
-                                texture: tile.texture
-                            });
-                        }
-                    }
-                    delete tile.grid;
-                    return tiles;
-                }, []);
-            }
         }
         resolve();
     })
