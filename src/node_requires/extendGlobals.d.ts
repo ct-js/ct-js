5656492d865a2856a4b6e41c40a070e6377f0f6c
Cosmo Myzrail Gorynych <admin@nersta.ru>
2022-11-07 17:00:32 +1200

    :bug: Fix inability to open a recently closed project in the same window

--- src/node_requires/extendGlobals.d.ts
+++ src/node_requires/extendGlobals.d.ts
@@ -17,6 +17,8 @@ declare global {
     function showOpenDialog(options: any): Promise<string | false>;
     function showSaveDialog(options: any): Promise<string | false>;
     interface Window {
+        path: string;
+        id: number;
         signals: any;
         orders: any;
         alertify: any;
