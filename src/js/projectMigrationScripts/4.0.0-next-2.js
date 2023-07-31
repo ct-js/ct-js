8c2209dcbf75d89f244cc337103c1fbcf4b98e10
Cosmo Myzrail Gorynych <admin@nersta.ru>
2023-08-01 00:22:51 +1200

    :zap: :construction: Use emitters' v3 format, make their behaviors a tuple type for easier editing

--- src/js/projectMigrationScripts/4.0.0-next-2.js
+++ src/js/projectMigrationScripts/4.0.0-next-2.js
@@ -2,7 +2,147 @@ window.migrationProcess = window.migrationProcess || [];
 
 window.migrationProcess.push({
     version: '4.0.0-next-2',
+    // eslint-disable-next-line max-lines-per-function, complexity
     process: project => new Promise(resolve => {
+        // pixi-particles -> @pixi/particle-emitter migration
+        for (const tandem of project.emitterTandems) {
+            const newEmitters = [];
+            for (let i = 0; i < tandem.emitters.length; i++) {
+                const v2Emitter = tandem.emitters[i],
+                      v2Settings = v2Emitter.settings;
+                const usesAcceleration = v2Emitter.acceleration.x !== 0 ||
+                                         v2Emitter.acceleration.y !== 0;
+                const v2Shape = v2Settings.spawnType;
+                let shapeConfig, shapeType;
+                switch (v2Shape) {
+                default:
+                case 'point':
+                    shapeType = 'spawnShape';
+                    shapeConfig = {
+                        type: 'torus',
+                        // eslint-disable-next-line id-blacklist
+                        data: {
+                            innerRadius: 0,
+                            radius: 0,
+                            x: 0,
+                            y: 0,
+                            rotation: Boolean(!v2Settings.noRotation)
+                        }
+                    };
+                    break;
+                case 'circle':
+                case 'ring':
+                    shapeType = 'spawnShape';
+                    shapeConfig = {
+                        type: 'torus',
+                        // eslint-disable-next-line id-blacklist
+                        data: {
+                            innerRadius: v2Shape === 'circle' ? 0 : v2Settings.spawnCircle.minR,
+                            radius: v2Settings.spawnCircle.minR,
+                            x: 0,
+                            y: 0,
+                            rotation: Boolean(!v2Settings.noRotation)
+                        }
+                    };
+                    break;
+                case 'rect':
+                    shapeType = 'spawnShape';
+                    shapeConfig = {
+                        type: 'rect',
+                        // eslint-disable-next-line id-blacklist
+                        data: {
+                            x: 0,
+                            y: 0,
+                            width: v2Settings.spawnRect.w,
+                            height: v2Settings.spawnRect.h
+                        }
+                    };
+                    break;
+                case 'burst':
+                    shapeType = 'spawnBurst';
+                    shapeConfig = {
+                        spacing: v2Settings.particleSpacing,
+                        start: v2Settings.angleStart,
+                        distance: 0
+                    };
+                    break;
+                }
+                const v3Emitter = {
+                    uid: v2Emitter.uid,
+                    texture: v2Emitter.texture,
+                    openedTabs: v2Emitter.openedTabs,
+                    settings: {
+                        frequency: v2Settings.frequency,
+                        lifetime: v2Settings.lifetime,
+                        spawnChance: v2Settings.spawnChance,
+                        emitterLifetime: v2Settings.emitterLifetime,
+                        maxParticles: v2Settings.maxParticles,
+                        addAtBack: v2Settings.addAtBack,
+                        particlesPerWave: v2Settings.particlesPerWave ?? 1,
+                        pos: {
+                            x: v2Settings.pos.x,
+                            y: v2Settings.pos.y
+                        },
+                        behaviors: [{ // 0
+                            type: 'alpha',
+                            config: {
+                                alpha: v2Settings.alpha.list
+                            }
+                        },
+                        { // 1
+                            type: 'color',
+                            config: v2Settings.color.list
+                        },
+                        { // 2
+                            type: 'blendMode',
+                            config: {
+                                blendMode: v2Settings.blendMode
+                            }
+                        },
+                        { // 3
+                            type: 'scale',
+                            config: v2Settings.scale.list
+                        },
+                        { // 4
+                            type: usesAcceleration ? 'moveAcceleration' : 'moveSpeed',
+                            config: usesAcceleration ?
+                                {
+                                    minStart: v2Settings.speed.list[0].value *
+                                             (v2Settings.minimumSpeedMultiplier ?? 1),
+                                    maxStart: v2Settings.speed.list[0].value,
+                                    accel: {
+                                        x: v2Settings.acceleration.x,
+                                        y: v2Settings.acceleration.y
+                                    },
+                                    rotate: Boolean(!v2Settings.noRotation),
+                                    maxSpeed: v2Settings.maxSpeed
+                                } :
+                                {
+                                    speed: v2Settings.speed.list,
+                                    minMult: v2Settings.minimumSpeedMultiplier ?? 1
+                                }
+                        },
+                        { // 5
+                            type: shapeType,
+                            config: shapeConfig
+                        },
+                        { // 6
+                            type: 'rotation',
+                            config: {
+                                minStart: v2Settings.startRotation.min,
+                                maxStart: v2Settings.startRotation.max,
+                                minSpeed: v2Settings.rotationSpeed.min,
+                                maxSpeed: v2Settings.rotationSpeed.max,
+                                accel: v2Settings.rotationAcceleration
+                            }
+                        }]
+                    }
+                };
+                tandem.emitters[i] = v3Emitter;
+            }
+        }
+
+        // Unified asset folders
         const oldGroups = {};
         // Flatten old groups into one group map
         for (const key in project.groups) {
--- src/node_requires/resources/emitterTandems/types.d.ts
+++ src/node_requires/resources/emitterTandems/types.d.ts
@@ -1,79 +1,69 @@
+import {EmitterConfigV3} from 'node_modules/@pixi/particle-emitter';
+import type {AccelerationBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/AccelerationMovement.d.ts';
+import type {AlphaBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/Alpha.d.ts';
+import type {ColorBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/Color.d.ts';
+import type {BlendModeBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/BlendMode.d.ts';
+import type {RotationBehavior, NoRotationBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/Rotation.d.ts';
+import type {ScaleBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/Scale.d.ts';
+import type {SpeedBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/SpeedMovement.d.ts';
+import type {ShapeSpawnBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/ShapeSpawn.d.ts';
+import type {BurstSpawnBehavior} from 'node_modules/@pixi/particle-emitter/lib/behaviors/BurstSpawn.d.ts';
+
 type EmitterTabsNames = 'texture' | 'colors' | 'scaling' | 'velocity' | 'gravity' |
                         'direction' | 'rotation' | 'spawningHeading' | 'shape';
 
 type EmitterBlendModes = 'normal' | 'multiply' | 'screen' | 'add';
-
-type EmitterSpawnTypes = 'point' | 'rect' | 'circle' | 'ring' | 'burst';
 
 interface ITimeSeries<type> {
     value: type,
     time: number
 }
 
+interface IEmitterConfigTupled extends EmitterConfigV3 {
+    behaviors: [{ // 0
+        type: 'alpha',
+        config: ConstructorParameters<typeof AlphaBehavior>[0]
+    },
+    { // 1
+        type: 'color',
+        config: ConstructorParameters<typeof ColorBehavior>[0]
+    },
+    { // 2
+        type: 'blendMode',
+        config: ConstructorParameters<typeof BlendModeBehavior>[0]
+    },
+    { // 3
+        type: 'scale',
+        config: ConstructorParameters<typeof ScaleBehavior>[0]
+    },
+    { // 4
+        type: 'moveSpeed',
+        config: ConstructorParameters<typeof SpeedBehavior>[0]
+    } | {
+        type: 'moveAcceleration',
+        config: ConstructorParameters<typeof AccelerationBehavior>[0]
+    },
+    { // 5
+        type: 'spawnShape',
+        config: ConstructorParameters<typeof ShapeSpawnBehavior>[0]
+    } | {
+        type: 'spawnBurst',
+        config: ConstructorParameters<typeof BurstSpawnBehavior>[0]
+    },
+    { // 6
+        type: 'rotation',
+        config: ConstructorParameters<typeof RotationBehavior>[0]
+    } | {
+        type: 'noRotation',
+        config: ConstructorParameters<typeof NoRotationBehavior>[0]
+    }]
+}
+
 interface ITandemEmitter {
     uid: string;
     texture: assetRef,
     openedTabs: Array<EmitterTabsNames>,
-    settings: {
-        alpha: {
-            list: Array<ITimeSeries<number>>,
-            isStepped: boolean
-        },
-        scale: {
-            list: Array<ITimeSeries<number>>,
-            isStepped: boolean
-        },
-        color: {
-            list: Array<ITimeSeries<string>>,
-            isStepped: boolean
-        },
-        blendMode: EmitterBlendModes,
-        speed: {
-            list: Array<ITimeSeries<number>>,
-            isStepped: boolean
-        },
-        startRotation: {
-            min: number,
-            max: number
-        },
-        rotationSpeed: {
-            min: number,
-            max: number
-        },
-        rotationAcceleration: number,
-        lifetime: {
-            min: number,
-            max: number
-        },
-        frequency: number,
-        spawnChance: number,
-        particlesPerWave: number,
-        angleStart: number,
-        emitterLifetime: number,
-        maxParticles: number,
-        maxSpeed: number,
-        pos: {
-            x: number,
-            y: number
-        },
-        acceleration: {
-            x: number,
-            y: number
-        },
-        addAtBack: boolean,
-        spawnType: EmitterSpawnTypes,
-        spawnCircle?: {
-            x: number,
-            y: number,
-            r: number,
-            minR?: number
-        },
-        spawnRect?: {
-            w: number,
-            h: number
-        },
-        delay: number
-    }
+    settings: IEmitterConfigTupled
 }
 
 interface ITandem extends IAsset {
