b2f50f4bb44a63a8ad41dd618b762f95315347c4
markmehere <52765023+markmehere@users.noreply.github.com>
2023-09-24 12:02:15 +1000

    :zap: styleUtils to TypeScript and new interface

--- src/node_requires/resources/styles/IStyle.d.ts
+++ src/node_requires/resources/styles/IStyle.d.ts
@@ -1,4 +1,30 @@
 declare interface IStyle extends IAsset {
     name: string;
-    // TODO:
+    font: {
+        family: string;
+        size: number;
+        italic?: boolean;
+        weight: string;
+        halign: string;
+        lineHeight?: number;
+        wrap?: boolean;
+        wrapPosition?: number;
+    };
+    fill?: {
+        type: string;
+        color?: string;
+        color1?: string;
+        color2?: string;
+        gradtype?: string;
+    };
+    stroke?: {
+        weight: number;
+        color: string;
+    };
+    shadow?: {
+        x: number;
+        y: number;
+        blur: number;
+        color: string | number;
+    }
 }
