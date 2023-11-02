5678fb78161bc5a30289ea119715c784a294c6f0
abhinav <abhinav@piperplay.co>
2023-11-03 00:29:01 +0530

    matter catmod gravity setting

--- app/data/ct.libs/matter/injections/beforeroomoncreate.js
+++ app/data/ct.libs/matter/injections/beforeroomoncreate.js
@@ -1,8 +1,16 @@
 if (this === ct.room) {
     ct.room.matterEngine = Matter.Engine.create();
     ct.room.matterWorld = ct.room.matterEngine.world;
-    ct.room.matterGravity = ct.room.matterGravity || [0, 9.8];
-    [ct.room.matterWorld.gravity.x, ct.room.matterWorld.gravity.y] = ct.room.matterGravity;
+    ct.room.matterGravity = ct.room.matterGravity || [
+        [
+            /*%matterGravity%*/
+        ][0][0],
+        [
+            /*%matterGravity%*/
+        ][0][1],
+    ];
+    [ct.room.matterWorld.gravity.x, ct.room.matterWorld.gravity.y] =
+        ct.room.matterGravity;
     ct.matter.rulebookStart = [];
     ct.matter.rulebookActive = [];
     ct.matter.rulebookEnd = [];
