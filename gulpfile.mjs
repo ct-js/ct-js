9b75695283fd358bff8eda3b7576dae96235578f
Cosmo Myzrail Gorynych <admin@nersta.ru>
2024-05-12 16:04:58 +1200

    :construction: Make new MacOS ARM binaries maybeee?

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -29,7 +29,6 @@ import spawnise from './node_requires/spawnise/index.js';
 import execute from './node_requires/execute.js';
 import i18n from './node_requires/i18n/index.js';
 
-import nwBuilderArm from './node_modules/nw-builder-arm/lib/index.cjs';
 import nwBuilder from './node_modules/nw-builder/lib/index.cjs';
 import resedit from 'resedit-cli';
 
@@ -148,13 +147,6 @@ const compileRiot = () =>
     .pipe(riot(riotSettings))
     .pipe(concat('riotTags.js'))
     .pipe(gulp.dest('./temp/'));
-
-const compileRiotPartial = path => {
-    console.log(`Updating tag at ${path}…`);
-    return gulp.src(path)
-    .pipe(riot(riotSettings))
-    .pipe(gulp.dest('./app/data/hotLoadTags/'));
-};
 
 const concatScripts = () =>
     streamQueue(
@@ -425,13 +417,13 @@ const processToPlatformMap = {
 };
 const launchApp = () => {
     const platformKey = `${process.platform}-${process.arch}`;
-    const NwBuilder = (platformKey === 'darwin-arm64') ? nwBuilderArm : nwBuilder;
+    const NwBuilder = nwBuilder;
     if (!(platformKey in processToPlatformMap)) {
         throw new Error(`Combination of OS and architecture ${process.platform}-${process.arch} is not supported by NW.js.`);
     }
     const nw = new NwBuilder({
         files: nwFiles,
-        version: platformKey === 'darwin-arm64' ? nwArmVersion : nwVersion,
+        version: nwVersion,
         platforms: [processToPlatformMap[platformKey]],
         flavor: 'sdk'
     });
@@ -491,7 +483,7 @@ export const bakePackages = async () => {
     await fs.remove(path.join('./build', `ctjs - v${pack.version}`));
     const nw = new NwBuilder({
         files: nwFiles,
-        platforms: platforms.filter(x => x !== 'osxarm'),
+        platforms,
         version: nwVersion,
         flavor: 'sdk',
         buildType: 'versioned',
@@ -500,38 +492,6 @@ export const bakePackages = async () => {
         macIcns: nightly ? './buildAssets/nightly.icns' : './buildAssets/icon.icns'
     });
     await nw.build();
-
-    if (platforms.indexOf('osxarm') > -1) {
-        try {
-            const NwBuilderArm = nwBuilderArm;
-            const nwarm = new NwBuilderArm({
-                files: nwFiles,
-                platforms: ['osxarm'],
-                version: nwVersion,
-                flavor: 'sdk',
-                buildType: 'versioned',
-                // forceDownload: true,
-                zip: false,
-                macIcns: nightly ? './buildAssets/nightly.icns' : './buildAssets/icon.icns'
-            });
-            await nwarm.build();
-        } catch (err) {
-            console.error(`
-    ╭──────────────────────────────────────────╮
-    │                                          ├──╮
-    │    Mac OS X (arm64) build failed! D:     │  │
-    │                                          │  │
-    │  The arm64 architecture on Mac OS X      │  │
-    │  relies upon unofficial builds. Thus it  │  │
-    │  may not always succeed. Other builds    │  │
-    │  will proceed.                           │  │
-    │                                          │  │
-    ╰─┬────────────────────────────────────────╯  │
-    ╰───────────────────────────────────────────╯
-    `);
-            platforms.splice(platforms.indexOf('osxarm'), 1);
-        }
-    }
 
     // Copy .itch.toml files for each target platform
     await Promise.all(platforms.map(platform => {
