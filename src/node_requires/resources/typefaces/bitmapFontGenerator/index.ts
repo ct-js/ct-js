56d2dcf970de25c3053d701528ebe99cd151c313
Cosmo Myzrail Gorynych <admin@nersta.ru>
2024-05-26 20:12:19 +0300

    :zap: Typescriptify bitmap font generator

--- src/node_requires/resources/typefaces/bitmapFontGenerator/index.ts
+++ src/node_requires/resources/typefaces/bitmapFontGenerator/index.ts
@@ -1,9 +1,33 @@
 /* eslint-disable max-len */
-const util = require('./util');
-const opentype = require('@konghayao/opentype.js');
+import util, {glyph} from './util';
+import opentype, {Glyph, Font} from 'opentype.js';
 
-const draw = function draw(ctx, glyphList, descend, options) {
-    var dict = {};
+type generatorOptions = {
+    width: number,
+    height: number,
+    baseline: number,
+    margin: number,
+    fill: string,
+    stroke: string,
+    list: string,
+    missingGlyph?: HTMLCanvasElement | string,
+    pixelPerfect: boolean,
+    fontOrigname: string,
+    typefaceName: string
+}
+
+const draw = (
+    ctx: CanvasRenderingContext2D,
+    glyphList: glyph[],
+    descend: number,
+    options: generatorOptions
+) => {
+    var dict: Record<number, {
+        x: number,
+        y: number,
+        width: number,
+        height: number
+    }> = {};
 
     var drawX = 1;
     var drawY = 1;
@@ -15,7 +39,7 @@ const draw = function draw(ctx, glyphList, descend, options) {
             if (options.width !== void 0) {
                 g.width = options.width;
             }
-            ctx.drawImage(options.missingGlyph, drawX, drawY, g.width, drawHeight);
+            ctx.drawImage(options.missingGlyph as HTMLCanvasElement, drawX, drawY, g.width, drawHeight);
             mg = {
                 x: drawX, y: drawY, width: g.width, height: drawHeight
             };
@@ -54,57 +78,68 @@ const draw = function draw(ctx, glyphList, descend, options) {
 };
 
 // eslint-disable-next-line max-lines-per-function, complexity
-export const generateBitmapFont = async function generateBitmapFont(fontSrc, outputPath, options, callback) {
+export const generateBitmapFont = async function generateBitmapFont(
+    fontSrc: string,
+    outputPath: string,
+    options: generatorOptions
+) {
     const fs = require('fs-extra');
     const buffer = await fs.readFile(fontSrc);
     const font = opentype.parse(buffer.buffer);
 
     if (!options.list || options.list.length === 0) {
-        options.list = Object.keys(font.glyphs.glyphs)
-        .map(code => String.fromCharCode(font.glyphs.glyphs[code].unicode) || ' ')
+        // Get private values
+        options.list = Object.keys((font.glyphs as any).glyphs)
+        .map(code => String.fromCharCode((font.glyphs as any).glyphs[code].unicode) || ' ')
         .join('');
     }
 
 
-    var lostChars = [];
-    var glyphList = [];
+    var lostChars: string[] = [];
+    var glyphList: Partial<glyph>[] = [];
 
     Array.from(options.list).forEach((char) => {
-        const [glyph] = font.stringToGlyphs(char);
-        glyph.font = font;
+        const glyph = Object.assign(font.stringToGlyphs(char)[0], {
+            font
+        });
         if (glyph.unicodes.length === 0) {
             lostChars.push(char);
         }
         const scale = 1 / font.unitsPerEm * options.height;
         glyphList.push({
             glyph,
-            width: Math.ceil(glyph.advanceWidth * scale)
+            width: Math.ceil((glyph.advanceWidth ?? 0) * scale)
         });
     });
 
     if (isNaN(options.baseline)) {
-        options.baseline = util.getMaxBaseline(glyphList, options.height);
+        options.baseline = util.getMaxBaseline(glyphList as glyph[], options.height);
     }
 
     // Update baseline value while adding missingGlyph to glyphList
     if (options.missingGlyph === void 0 || typeof options.missingGlyph === 'string') {
+        // Get private values
         // eslint-disable-next-line prefer-destructuring
-        var g = font.glyphs.glyphs[0];
+        var g: Glyph & {
+            font: Font
+        } = (font.glyphs as any).glyphs[0];
         if (options.missingGlyph) {
-            g = font.charToGlyph(options.missingGlyph);
+            g = font.charToGlyph(options.missingGlyph) as Glyph & {
+                font: Font
+            };
         }
         var scale = 1 / font.unitsPerEm * options.height;
         g.font = font;
         glyphList.push({
             glyph: g,
-            width: Math.ceil(g.advanceWidth * scale)
+            width: Math.ceil((g.advanceWidth ?? 0) * scale)
         });
         if (options.baseline < (g.yMax || 0) * scale) {
             options.baseline = Math.ceil((g.yMax || 0) * scale);
         }
     }
 
-    var descend = util.getMinDescend(glyphList, options.height);
+    var descend = util.getMinDescend(glyphList as glyph[], options.height);
     var adjustedHeight = util.getAdjustedHeight(descend, options.height, options.baseline);
 
     // Calculate the required canvas size
@@ -112,7 +147,7 @@ export const generateBitmapFont = async function generateBitmapFont(fontSrc, out
     if (options.width === void 0) {
         canvasSize = util.calculateCanvasSizeProp(
             options.list,
-            glyphList,
+            glyphList as glyph[],
             adjustedHeight,
             options.baseline + descend,
             options.margin || 1
@@ -141,16 +176,16 @@ export const generateBitmapFont = async function generateBitmapFont(fontSrc, out
         // because otherwise pixel fonts will miss some of their pieces due to rounding errors
         canvas.width = canvasSize.width * 2;
         canvas.height = canvasSize.height * 2;
-        ctx = canvas.getContext('2d');
+        ctx = canvas.getContext('2d')!;
         ctx.save();
         ctx.scale(2, 2);
-        drawResult = draw(ctx, glyphList, descend, options);
+        drawResult = draw(ctx, glyphList as glyph[], descend, options);
         ctx.restore();
     } else {
         canvas.width = canvasSize.width;
         canvas.height = canvasSize.height;
-        ctx = canvas.getContext('2d');
-        drawResult = draw(ctx, glyphList, descend, options);
+        ctx = canvas.getContext('2d')!;
+        drawResult = draw(ctx, glyphList as glyph[], descend, options);
     }
 
     // Notify about characters that could not be drawn
@@ -165,11 +200,11 @@ export const generateBitmapFont = async function generateBitmapFont(fontSrc, out
         downscaleCanvas = document.createElement('canvas');
         downscaleCanvas.width = canvas.width / 2;
         downscaleCanvas.height = canvas.height / 2;
-        const downscalex = downscaleCanvas.getContext('2d');
+        const downscalex = downscaleCanvas.getContext('2d')!;
         downscalex.drawImage(canvas, 0, 0, canvas.width / 2, canvas.height / 2);
-        await util.outputBitmapFont(outputPath, downscaleCanvas, callback);
+        await util.outputBitmapFont(outputPath, downscaleCanvas);
     } else {
-        await util.outputBitmapFont(outputPath, canvas, callback);
+        await util.outputBitmapFont(outputPath, canvas);
     }
     return {
         map: drawResult.map,
