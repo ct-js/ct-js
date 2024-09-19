5ff93c073614ac382b0316c8326abab0572f2bab
Cosmo Myzrail Gorynych <admin@nersta.ru>
2024-09-19 13:32:48 +1200

    :construction: Probably fix symlink issues on Windows?

--- devSetup.gulpfile.js
+++ devSetup.gulpfile.js
@@ -24,7 +24,7 @@ const cleanup = () => {
 
 const npmInstall = path => done => {
     console.log(`Running 'npm install' for ${path}…`);
-    spawnise.spawn((/^win/).test(process.platform) ? 'npm.cmd' : 'npm', ['install'], {
+    spawnise.spawn((/^win/).test(process.platform) ? 'npm.cmd' : 'npm', ['install', '--install-links'], {
         cwd: path || './',
         shell: true
     })
